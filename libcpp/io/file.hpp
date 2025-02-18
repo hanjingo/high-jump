c2e6381b368ba484a997dafe384f50bce406b8c9
hanjingo <hehehunanchina@live.com>
2025-02-18 10:39:27 +0800

    fix file size bug

---
+++ libcpp/io/file.hpp
@@ -0,0 +1,38 @@
+#ifndef FILE_HPP
+#define FILE_HPP
+
+#ifndef FSIZE
+#define FSIZE unsigned long long
+#endif
+    
+#ifndef BYTE
+#define BYTE(n) \
+    ((FSIZE)(n))
+#endif
+
+#ifndef KB
+#define KB(n) \
+    ((FSIZE)(n) * 0x400)
+#endif
+
+#ifndef MB
+#define MB(n) \
+    ((FSIZE)(n) * 0x100000)
+#endif
+
+#ifndef GB
+#define GB(n) \
+    ((FSIZE)(n) * 0x40000000)
+#endif
+
+#ifndef TB
+#define TB(n) \
+    ((FSIZE)(n) * 0x10000000000)
+#endif
+
+namespace libcpp
+{
+
+}
+
+#endif
--- tests/fsize_test.cpp
+++ tests/fsize_test.cpp
@@ -1,2 +1,27 @@
 #include <gtest/gtest.h>
-#include <libcpp/io/fsize.hpp>
+#include <libcpp/io/file.hpp>
+
+TEST(file, BYTE)
+{
+    ASSERT_EQ(BYTE(1), 1);
+}
+
+TEST(file, KB)
+{
+    ASSERT_EQ(KB(1), 1 * 1024);
+}
+
+TEST(file, MB)
+{
+    ASSERT_EQ(MB(1), 1 * 1024 * 1024);
+}
+
+TEST(file, GB)
+{
+    ASSERT_EQ(GB(1), 1 * 1024 * 1024 * 1024);
+}
+
+TEST(file, TB)
+{
+    ASSERT_EQ(TB(1) == (unsigned long long)(1) * 1024 * 1024 * 1024 * 1024, true);
+}

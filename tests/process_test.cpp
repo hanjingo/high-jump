1c13a2d96d3733205fa8ff60a58261c6532bf23a
hanjingo <hehehunanchina@live.com>
2025-02-19 16:41:30 +0800

    fix application, process, signal bug

---
+++ tests/process_test.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+#include <libcpp/os/process.hpp>
+
+// TEST(process, is_running)
+// {
+//     ASSERT_EQ(libcpp::process::is_running(libcpp::process::getpid()), true);
+// }
+
+TEST(process, child)
+{
+    ASSERT_EQ(libcpp::process::child("ping").valid(), true);
+}
+
+TEST(process, spawn)
+{
+    libcpp::process::spawn("ping");
+}
+
+TEST(process, system)
+{
+    libcpp::process::error_code_t ec;
+    libcpp::process::system("ping", ec);
+    ASSERT_EQ(ec.value() >= 0, true);
+}
+
+TEST(process, getpid)
+{
+    ASSERT_EQ(libcpp::process::getpid() > -1, true);
+}
+
+TEST(process, getppid)
+{
+    ASSERT_EQ(libcpp::process::getppid() > -1, true);
+}
+
+TEST(process, time_stamp)
+{
+    ASSERT_EQ(libcpp::process::time_stamp() > 0, true);
+}

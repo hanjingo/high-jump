135be97a20b9ca7b7ac3e0bd3fdf79afefe49388
hanjingo <hehehunanchina@live.com>
2025-02-21 19:43:54 +0800

    add options parse

--- example/env/main.cpp
+++ example/env/main.cpp
@@ -11,7 +11,24 @@ int main(int argc, char* argv[])
     std::cout << "SECONDS = " << COMPILE_SECONDS << std::endl;
     std::cout << "DATE_TIME = " << std::string(COMPILE_TIME) << std::endl;
 
-    std::cout << "getenv(\"PATH\") = " << libcpp::getenv("PATH") << std::endl;
+    libcpp::options opt;
+    opt.add<std::string>("name", std::string("not found"));
+    opt.add<int>("age", 100);
+    opt.add<float>("balance", 10000.12);
+    opt.add<std::string>("other", "nothing");
+
+    // ./env --name=harry --age=30 --balance=123.456
+    std::cout << "parse<std::string>(argc, argv, \"name\") = " 
+        << opt.parse<std::string>(argc, argv, "name") << std::endl; // harry
+
+    std::cout << "parse<int>(argc, argv, \"age\") = " 
+        << opt.parse<int>(argc, argv, "age") << std::endl; // 30
+
+    std::cout << "parse<float>(argc, argv, \"balance\") = " 
+        << opt.parse<float>(argc, argv, "balance") << std::endl; // 123.456
+
+    std::cout << "parse<float>(argc, argv, \"other\") = " 
+        << opt.parse<std::string>(argc, argv, "other") << std::endl; // nothing
     
     std::cin.get();
     return 0;
--- tests/env_test.cpp
+++ tests/env_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <libcpp/os/env.hpp>
 
-TEST(env, getenv)
+TEST(env, parse)
 {
+    ASSERT_EQ(true, true);
 }

b633da05fb2dda6cf96d4ee75d36f3999e2bae32
hanjingo <hehehunanchina@live.com>
2025-02-25 11:43:21 +0800

    fix error process bug

--- libcpp/os/process.hpp
+++ libcpp/os/process.hpp
@@ -1,57 +1,133 @@
 #ifndef PROCESS_HPP
 #define PROCESS_HPP
 
+#include <vector>
+#include <functional>
 #include <boost/process.hpp>
+#include <boost/filesystem.hpp>
 
 namespace libcpp
 {
-class process
+
+namespace process
 {
-public:
-    using pid_t = boost::process::pid_t;
-    using exit_code_t = int;
-    using error_code_t = std::error_code;
-    using child_t = boost::process::child;
+using pid_t         = boost::process::pid_t;
+using exit_code_t   = int;
+using error_code_t  = std::error_code;
+using child_t       = boost::process::child;
+using group_t       = boost::process::group;
+using path_t        = boost::filesystem::path;
 
-    enum state_t {
-        state_create,
-        state_ready,
-        state_running,
-        state_block,
-        state_stop,
-    };
+using ipstream_t    = boost::process::ipstream;
+using opstream_t    = boost::process::opstream;
 
-public:
-    explicit process() {}
-    ~process() { }
+// pair: [pid, process name]
+using list_match_cb = std::function<bool(const std::vector<std::string>)>;
 
-    template <typename... Args>
-    static child_t child(Args&&... args) 
-    { 
-        return boost::process::child(std::forward<Args>(args)...);
+static constexpr boost::process::detail::std_in_<void> std_in = boost::process::std_in;
+static constexpr boost::process::detail::std_out_<1> std_out  = boost::process::std_out;
+static constexpr boost::process::detail::std_out_<2> std_err  = boost::process::std_err;
+
+#if defined(WIN32)
+static constexpr char cmd_list_pid[] = "tasklist /FO CSV /NH";
+static constexpr char cmd_kill[]     = "taskkill /PID ";
+#else
+static constexpr char cmd_list_pid[] = "ps -eo comm,pid";
+static constexpr char cmd_kill[]     = "kill -9 ";
+#endif
+
+static pid_t getpid() { return pid_t(::getpid()); }
+
+static pid_t getppid() { return pid_t(::getppid()); }
+
+template <typename... Args>
+static int system(Args&&... args) 
+{ 
+    return boost::process::system(std::forward<Args>(args)...); 
+}
+
+template <typename... Args>
+static child_t child(Args&&... args) 
+{ 
+    return boost::process::child(std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+static void spawn(Args&&... args) 
+{ 
+    boost::process::spawn(std::forward<Args>(args)...);
+}
+
+template <typename... Args>
+static void daemon(Args&&... args)
+{
+    // Step1. ignore signal
+    // Step2. fork child and set work directory to pwd/root
+    // Step3. close file
+    // Step4. io_in, io_out, io_err redirect io null
+    boost::process::group gp{};
+    boost::process::spawn(
+        std::forward<Args>(args)..., 
+        boost::process::start_dir(boost::filesystem::current_path()), // pwd/root
+        boost::process::std_in.null(),
+        boost::process::std_out.null(),
+        boost::process::std_out.null(),
+        gp
+    );
+    gp.detach();
+}
+
+static group_t group()
+{
+    return boost::process::group();
+}
+
+static void list(std::vector<pid_t>& result, 
+                 list_match_cb match = [](const std::vector<std::string>) -> bool{return true;})
+{
+        boost::process::ipstream stream;
+        std::string line;
+        std::vector<std::string> vec;
+        bool is_first_line = true;
+    try{
+        boost::process::child proc(cmd_list_pid, boost::process::std_out > stream);
+        while (std::getline(stream, line)) 
+        {
+#if defined(WIN32)
+            boost::algorithm::split(vec, line, [](char c) { return c == ','; });
+            for (auto itr = vec.begin(); itr != vec.end(); ++itr)
+                boost::erase_all(*itr, "\"");
+#else
+            if (is_first_line) 
+            { 
+                is_first_line = false; 
+                continue; 
+            }
+            boost::algorithm::split(vec, line, boost::algorithm::is_space(), boost::token_compress_on);
+#endif
+            if (!match(vec) || vec.size() < 2)
+                continue;
+
+            result.push_back(pid_t(std::stoi(vec[1])));
+        }
+    } catch(const std::exception& e) {
+        return;
+    }
+}
+
+static void kill(const pid_t pid)
+{
+    try {
+        std::string cmd = std::string(cmd_kill).append(std::to_string(pid));
+        boost::process::child proc(cmd);
+        proc.wait();
+    } catch(const std::exception& e) {
+        return;
     }
 
-    template <typename... Args>
-    static void spawn(Args&&... args) 
-    { 
-        boost::process::spawn(std::forward<Args>(args)...);
-    }
+    return;
+}
 
-    template <typename... Args>
-    static int system(Args&&... args) 
-    { 
-        return boost::process::system(std::forward<Args>(args)...); 
-    }
-    
-    static pid_t getpid() { return ::getpid(); }
-
-    static pid_t getppid() { return ::getppid(); }
-
-    static int64_t time_stamp() 
-    { 
-        return std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::system_clock::now().time_since_epoch()).count();
-    }
 };
 
 }
--- libcpp/testing/error.hpp
+++ libcpp/testing/error.hpp
@@ -4,58 +4,79 @@
 #include <string>
 #include <iostream>
 #include <system_error>
-#include <unordered_map>
 
 namespace libcpp
 {
 
-template<typename Code>
+template<typename Value>
 class error
 {
-    using any_t = void*;
-
 public:
     error() = delete;
-    error(const error& rhs) : code_{rhs.code_} {}
-    error(Code code) : code_{code} {}
+    error(const error<Value>& rhs) : value_{rhs.value_} {}
+    error(Value value) : value_{value} {}
 
     ~error() {}
 
     inline error& operator=(const error& e)
     {
-        code_ = e.code_;
+        value_ = e.value_;
         return *this;
     }
 
-    inline error& operator=(const Code& arg)
+    inline error& operator=(const Value& arg)
     {
-        code_ = arg;
+        value_ = arg;
         return *this;
     }
 
     inline friend bool operator==(const error& e1, const error& e2)
     {
-        return e1.code_ == e2.code_;
+        return e1.value_ == e2.value_;
     }
 
     inline friend bool operator!=(const error& e1, const error& e2)
     {
-        return e1.code_ != e2.code_;
+        return e1.value_ != e2.value_;
     }
 
     inline friend std::ostream& operator<<(std::ostream& out, const error& e)
     {
-        out << e.code_;
+        out << e.value_;
         return out;
     }
 
-    inline Code code()
+    inline Value value()
     {
-        return code_;
+        return value_;
     }
 
+protected:
+    Value value_;
+};
+
+template<typename Value, typename Handler = void*>
+class error_v2 : public error<Value>
+{
+public:
+    error_v2() = delete;
+    error_v2(const error_v2<Value, Handler>& rhs) : error<Value>(rhs), handler_{(void*)(rhs.handler_)} {}
+    error_v2(Value value) : error<Value>(value), handler_{nullptr} {}
+    error_v2(Value value, Handler handler) : error<Value>(value), handler_{(void*)(handler)} {}
+    ~error_v2() {}
+
+    template<typename Ret = void, typename... Types>
+    Ret handle(Types&& ... args)
+    {
+        try {
+            auto fn = (Ret(*)(Types...))(handler_);
+            return fn(std::forward<Types>(args)...);
+        } catch (std::exception e) {
+            // DO NOTHING
+        }
+    }
 private:
-    Code code_;
+    void* handler_ = nullptr;
 };
 
 class error_factory
@@ -64,10 +85,16 @@ public:
     error_factory() {}
     ~error_factory() {}
 
-    template <typename Code = std::string>
-    static error<Code> create(Code code)
+    template <typename Value>
+    static error<Value> create(Value value)
     {
-        return error<Code>(code);
+        return error<Value>(value);
+    }
+
+    template <typename Value, typename Handler = void*>
+    static error_v2<Value, Handler> create(Value value, Handler handler)
+    {
+        return error_v2<Value, Handler>(value, (void*)(handler));
     }
 };
 

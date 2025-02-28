4dd0969db5590d2629cae3121d2c12b4a186822f
hanjingo <hehehunanchina@live.com>
2025-02-28 19:43:35 +0800

    fix log bug

--- example/log/main.cpp
+++ example/log/main.cpp
@@ -13,32 +13,42 @@ int main(int argc, char* argv[])
     LOG_CRITICAL("LOG_CRITICAL");
     LOG_FLUSH();
 
-    // do sync logging by user defined logger
+    // do sync logging by user defined rotate file logger
     auto lg1 = std::make_shared<libcpp::logger>("lg1");
-    lg1->add_sink(libcpp::logger::create_sink());
-    lg1->add_sink(libcpp::logger::create_sink("logs/libcpp.log", 2 * 1024 * 1024, 200000));
+    lg1->add_sink(libcpp::logger::create_stdout_sink());
+    lg1->add_sink(libcpp::logger::create_rotate_file_sink("logs/libcpp.log", 
+                                                          2 * 1024 * 1024, 
+                                                          2,
+                                                          false));
     lg1->set_pattern("%Y-%m-%d %H:%M:%S.%e [%-8l] %v");
     libcpp::logger::set_default(std::move(lg1));
-    LOG_TRACE("LOG_TRACE {}", "hello");
-    LOG_DEBUG("LOG_DEBUG d{0:d} 0x{0:x} o{0:o} b{0:b}", 10);
-    LOG_INFO("LOG_INFO {0:03.2f}", 123.45);
-    LOG_WARN("LOG_WARN {1} {0}", "hello", "world");
-    LOG_ERROR("LOG_ERROR");
-    LOG_CRITICAL("LOG_CRITICAL");
+    LOG_TRACE("lg1 LOG_TRACE {}", "hello");
+    LOG_DEBUG("lg1 LOG_DEBUG d{0:d} 0x{0:x} o{0:o} b{0:b}", 10);
+    LOG_INFO("lg1 LOG_INFO {0:03.2f}", 123.45);
+    LOG_WARN("lg1 LOG_WARN {1} {0}", "hello", "world");
+    LOG_ERROR("lg1 LOG_ERROR");
+    LOG_CRITICAL("lg1 LOG_CRITICAL");
     LOG_FLUSH();
 
-    // do async logging by user defined logger
+    // do sync logging by user defined daily logger
     auto lg2 = std::make_shared<libcpp::logger>("lg2", true);
-    lg2->add_sink(libcpp::logger::create_sink());
-    lg2->add_sink(libcpp::logger::create_sink("logs/libcpp.log", 2 * 1024 * 1024, 200000));
+    lg2->add_sink(libcpp::logger::create_stdout_sink());
+    lg2->clear_sink();
+    lg2->add_sink(libcpp::logger::create_daily_file_sink("logs/libcpp.log", 
+                                                         0, 
+                                                         1));
     libcpp::logger::set_default(std::move(lg2));
-    LOG_TRACE("LOG_TRACE {}", "hello");
-    LOG_DEBUG("LOG_DEBUG d{0:d} 0x{0:x} o{0:o} b{0:b}", 10);
-    LOG_INFO("LOG_INFO {0:03.2f}", 123.45);
-    LOG_WARN("LOG_WARN {1} {0}", "hello", "world");
-    LOG_ERROR("LOG_ERROR");
-    LOG_CRITICAL("LOG_CRITICAL");
-    LOG_FLUSH();
+    while (true)
+    {
+        LOG_TRACE("lg2 LOG_TRACE {}", "hello");
+        LOG_DEBUG("lg2 LOG_DEBUG d{0:d} 0x{0:x} o{0:o} b{0:b}", 10);
+        LOG_INFO("lg2 LOG_INFO {0:03.2f}", 123.45);
+        LOG_WARN("lg2 LOG_WARN {1} {0}", "hello", "world");
+        LOG_ERROR("lg2 LOG_ERROR");
+        LOG_CRITICAL("lg2 LOG_CRITICAL");
+        LOG_FLUSH();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
 
     std::cin.get();
     return 0;
---
+++ tests/string_util_test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include <libcpp/util/string_util.hpp>
+
+TEST(string_util, split)
+{
+    auto arr1 = libcpp::string_util::split("abc;123;++", ";");
+    ASSERT_EQ(arr1.size(), 3);
+
+    auto arr2 = libcpp::string_util::split("broadcast,database,quote,sentinel", ",");
+    ASSERT_EQ(arr2[0] == "broadcast", true);
+    ASSERT_EQ(arr2[1] == "database", true);
+    ASSERT_EQ(arr2[2] == "quote", true);
+    ASSERT_EQ(arr2[3] == "sentinel", true);
+}
+
+TEST(string_util, equal)
+{
+    ASSERT_EQ(libcpp::string_util::equal("hello", "hello"), true);
+}
+
+TEST(string_util, from_wchar)
+{
+    // ASSERT_STREQ(libcpp::string_util::from_wchar(libcpp::string_util::to_wchar(std::string("abc"))).c_str(), "abc");
+}
+
+TEST(string_util, from_wstring)
+{
+    ASSERT_STREQ(libcpp::string_util::from_wstring(libcpp::string_util::to_wstring(std::string("hello"))).c_str(), "hello");
+}
+
+TEST(string_util, fmt)
+{
+    ASSERT_EQ(libcpp::string_util::fmt("{}-{}", "hello", "world") == std::string("hello-world"), true);
+    ASSERT_EQ(libcpp::string_util::fmt("{1}-{0}", "hello", "world") == std::string("world-hello"), true);
+}

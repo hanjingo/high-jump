2a59ee1d97eaefe164772436187c76a1aa9e00d6
hanjingo <hehehunanchina@live.com>
2025-03-10 22:38:08 +0800

    improve log,dbuffer performance

--- example/log/main.cpp
+++ example/log/main.cpp
@@ -21,14 +21,13 @@ int main(int argc, char* argv[])
                                                           2,
                                                           false));
     lg1->set_pattern("%Y-%m-%d %H:%M:%S.%e [%-8l] %v");
-    libcpp::logger::set_default(std::move(lg1));
-    LOG_TRACE("lg1 LOG_TRACE {}", "hello");
-    LOG_DEBUG("lg1 LOG_DEBUG d{0:d} 0x{0:x} o{0:o} b{0:b}", 10);
-    LOG_INFO("lg1 LOG_INFO {0:03.2f}", 123.45);
-    LOG_WARN("lg1 LOG_WARN {1} {0}", "hello", "world");
-    LOG_ERROR("lg1 LOG_ERROR");
-    LOG_CRITICAL("lg1 LOG_CRITICAL");
-    LOG_FLUSH();
+    lg1->trace("lg1 LOG_TRACE {}", "hello");
+    lg1->debug("lg1 LOG_DEBUG d{0:d} 0x{0:x} o{0:o} b{0:b}", 10);
+    lg1->info("lg1 LOG_INFO {0:03.2f}", 123.45);
+    lg1->warn("lg1 LOG_WARN {1} {0}", "hello", "world");
+    lg1->error("lg1 LOG_ERROR");
+    lg1->critical("lg1 LOG_CRITICAL");
+    lg1->flush();
 
     // do sync logging by user defined daily logger
     auto lg2 = std::make_shared<libcpp::logger>("lg2", true);
@@ -37,16 +36,15 @@ int main(int argc, char* argv[])
     lg2->add_sink(libcpp::logger::create_daily_file_sink("logs/libcpp.log", 
                                                          0, 
                                                          1));
-    libcpp::logger::set_default(std::move(lg2));
     while (true)
     {
-        LOG_TRACE("lg2 LOG_TRACE {}", "hello");
-        LOG_DEBUG("lg2 LOG_DEBUG d{0:d} 0x{0:x} o{0:o} b{0:b}", 10);
-        LOG_INFO("lg2 LOG_INFO {0:03.2f}", 123.45);
-        LOG_WARN("lg2 LOG_WARN {1} {0}", "hello", "world");
-        LOG_ERROR("lg2 LOG_ERROR");
-        LOG_CRITICAL("lg2 LOG_CRITICAL");
-        LOG_FLUSH();
+        lg2->trace("lg2 LOG_TRACE {}", "hello");
+        lg2->debug("lg2 LOG_DEBUG d{0:d} 0x{0:x} o{0:o} b{0:b}", 10);
+        lg2->info("lg2 LOG_INFO {0:03.2f}", 123.45);
+        lg2->warn("lg2 LOG_WARN {1} {0}", "hello", "world");
+        lg2->error("lg2 LOG_ERROR");
+        lg2->critical("lg2 LOG_CRITICAL");
+        lg2->flush();
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 
--- tests/dbuffer.cpp
+++ tests/dbuffer.cpp
@@ -23,30 +23,59 @@ TEST(dbuffer, read)
     for (int i = 0; i < buf3.size(); ++i)
         ASSERT_EQ(buf3[i], buf2[i]);
 
-    int times = 100000000;
-    auto start = std::chrono::system_clock::now();
-    for (int n = 0; n < times; ++n)
-    {
-        dbuf.write(n);
-        dbuf.read(j);
-    }
-    auto end = std::chrono::system_clock::now();
-    std::cout << "dbuffer time passed:" << (end - start).count() << std::endl;
+    int times = 100000;
+    auto dbuffer_total_start = std::chrono::system_clock::now();
+    std::thread t1([&](){
+        auto start = std::chrono::system_clock::now();
+        for (int n = 0; n < times; ++n)
+        {
+            dbuf.write(buf1);
+        }
+        auto end = std::chrono::system_clock::now();
+        std::cout << "dbuffer write time passed:" << (end - start).count() << std::endl;
+    });
+    std::thread t2([&](){
+        auto start = std::chrono::system_clock::now();
+        for (int n = 0; n < times; ++n)
+        {
+            dbuf.read(buf2);
+        }
+        auto end = std::chrono::system_clock::now();
+        std::cout << "dbuffer read time passed:" << (end - start).count() << std::endl;
+    });
+    t1.join();
+    t2.join();
+    auto dbuffer_total_end = std::chrono::system_clock::now();
+    std::cout << "dbuffer total read/write time passed:" << (dbuffer_total_end - dbuffer_total_start).count() << std::endl;
 
     std::shared_mutex mu;
-    start = std::chrono::system_clock::now();
-    for (int n = 0; n < times; ++n)
-    {
-        mu.lock();
-        i = n; // write
-        mu.unlock();
-
-        mu.lock_shared();
-        j = i; // read
-        mu.unlock_shared();
-    }
-    end = std::chrono::system_clock::now();
-    std::cout << "rwmutex time passed:" << (end - start).count() << std::endl;
+    auto rwlock_total_start = std::chrono::system_clock::now();
+    std::thread t3([&](){
+        auto start = std::chrono::system_clock::now();
+        for (int n = 0; n < times; ++n)
+        {
+            mu.lock();
+            buf3 = buf1; // write
+            mu.unlock();
+        }
+        auto end = std::chrono::system_clock::now();
+        std::cout << "rwlock write time passed:" << (end - start).count() << std::endl;
+    });
+    std::thread t4([&](){
+        auto start = std::chrono::system_clock::now();
+        for (int n = 0; n < times; ++n)
+        {
+            mu.lock_shared();
+            buf2 = buf3; // read
+            mu.unlock_shared();
+        }
+        auto end = std::chrono::system_clock::now();
+        std::cout << "rwlock read time passed:" << (end - start).count() << std::endl;
+    });
+    t3.join();
+    t4.join();
+    auto rwlock_total_end = std::chrono::system_clock::now();
+    std::cout << "rwlock total read/write time passed:" << (rwlock_total_end - rwlock_total_start).count() << std::endl;
 }
 
 TEST(dbuffer, write)

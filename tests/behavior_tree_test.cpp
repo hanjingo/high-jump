da9a4ec879f928f05f5a666fbddfc991833fcdf4
hanjingo <hehehunanchina@live.com>
2025-04-06 03:16:28 +0000

    add safe_map safe_vector

--- tests/behavior_tree_test.cpp
+++ tests/behavior_tree_test.cpp
@@ -1,2 +1,62 @@
 #include <gtest/gtest.h>
 // #include <libcpp/algo/behavior_tree.hpp>
+
+//     static const char* xml_text = R"(
+//     <root libcppCPP_format="4" >
+//         <BehaviorTree ID="MainTree">
+//            <Sequence name="root">
+//                <AlwaysSuccess/>
+//                <say_something   message="this works too" />
+//                <think_what_to_say text="{the_answer}"/>
+//                <say_something   message="{the_answer}" />
+//            </Sequence>
+//         </BehaviorTree>
+//     </root>
+//     )";
+   
+//    class say_something : public libcpp::sync_action_node
+//    {
+//    public:
+//        say_something(const std::string& name, const libcpp::node_configuration& config) :
+//            libcpp::sync_action_node(name, config)
+//        {}
+   
+//        libcpp::node_status tick() override
+//        {
+//            std::string msg;
+//            getInput("message", msg);
+//            std::cout << "say_something: " << msg << std::endl;
+//            return libcpp::node_status::SUCCESS;
+//        }
+   
+//        static libcpp::ports_list provided_ports()
+//        {
+//            return {libcpp::input_port<std::string>("message")};
+//        }
+//    };
+   
+//    class think_what_to_say : public libcpp::sync_action_node
+//    {
+//    public:
+//        think_what_to_say(const std::string& name, const libcpp::node_configuration& config) :
+//            libcpp::sync_action_node(name, config)
+//        {}
+   
+//        libcpp::node_status tick() override
+//        {
+//            setOutput("text", "The answer is 42");
+//            return libcpp::node_status::SUCCESS;
+//        }
+   
+//        static libcpp::ports_list provided_ports()
+//        {
+//            return {libcpp::output_port<std::string>("text")};
+//        }
+//    };
+
+//    TEST(behavior_tree, register_nodes)
+//    {
+//        libcpp::behavior_tree_factory factory;
+//        factory.registerNodeType<say_something>("say_something");
+//        factory.registerNodeType<think_what_to_say>("think_what_to_say");
+//    }
--- tests/trie_test.cpp
+++ tests/trie_test.cpp
@@ -1,7 +1,44 @@
 #include <gtest/gtest.h>
 #include <libcpp/algo/trie.hpp>
 
-TEST(trie, htrie_map)
+TEST(trie, insert)
 {
     libcpp::htrie_map<char, std::int64_t> map;
+    map.insert("hello", 1);
+    map.insert("world", 2);
+    map.insert("hell", 3);
+    map.insert("helloo", 4);
+    map.insert("hellooo", 5);
+    map.insert("helloooo", 6);
+    map.insert("hellooooo", 7);
+    map.insert("helloooooo", 8);
+    map.insert("hellooooooo", 9);
+    map.insert("helloooooooo", 10);
+
+    ASSERT_EQ(map.size(), 10);
+
+    // std::int64_t value;
+    // ASSERT_EQ(map.find("hello", value), true);
+    // ASSERT_EQ(value, 1);
+
+    // ASSERT_EQ(map.find("world", value), true);
+    // ASSERT_EQ(value, 2);
+
+    // ASSERT_EQ(map.find("hell", value), true);
+    // ASSERT_EQ(value, 3);
+
+    // ASSERT_EQ(map.find("helloo", value), true);
+    // ASSERT_EQ(value, 4);
+
+    // ASSERT_EQ(map.find("hellooo", value), true);
+    // ASSERT_EQ(value, 5);
+
+    // ASSERT_EQ(map.find("helloooo", value), true);
+    // ASSERT_EQ(value, 6);
+
+    // ASSERT_EQ(map.find("hellooooo", value), true);
+    // ASSERT_EQ(value, 7);
+
+    // ASSERT_EQ(map.find("helloooooo", value), true);
+    // ASSERT_EQ(value, 8);
 }

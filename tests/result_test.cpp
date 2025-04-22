ec56d19dea7279871039e1495f708d28af73d89a
hanjingo <hehehunanchina@live.com>
2025-04-22 16:03:20 +0000

    result + clickhouse

---
+++ tests/result_test.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <libcpp/testing/result.hpp>
+
+enum error1 : int 
+{
+    ok,
+    fail,
+    unknow
+};
+
+TEST(result, result)
+{
+    auto fn = []()->libcpp::result<error1>{
+        return error1::fail;
+    };
+
+    auto ret = fn();
+    // ASSERT_EQ(ret == error1::fail, true);
+}

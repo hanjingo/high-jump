e0ebdafd987c8ba9e606dee477239fc7cff99529
hanjingo <hehehunanchina@live.com>
2025-09-24 20:01:06 +0800

    fix example/cmake bug

--- examples/sdk/src/api.cpp
+++ examples/sdk/src/api.cpp
@@ -1,6 +1,5 @@
 #include "api.h"
 
-#include <hj/testing/crash.hpp>
 #include <hj/util/once.hpp>
 
 C_STYLE_EXPORT void version(context ctx)
@@ -16,10 +15,6 @@ C_STYLE_EXPORT void init(context ctx)
     ONCE(
         if (sizeof(ctx) != ctx.sz)
             return;
-
-        // add crash handle support
-        hj::crash_handler::instance()->prevent_set_unhandled_exception_filter();
-        hj::crash_handler::instance()->set_local_path("./");
 
         // add your code here ...
     )

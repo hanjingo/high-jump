39158b8cdbcab99ae2aff81dcce7b0d100c9b587
hanjingo <hehehunanchina@live.com>
2025-10-15 17:11:42 +0800

    add bench files

---
+++ benchs/crc16_bench.cpp
@@ -0,0 +1,82 @@
+#include <benchmark/benchmark.h>
+#include <hj/algo/crc16.h>
+#include <cstring>
+#include <string>
+
+static void bm_crc16_standard_test_vector(benchmark::State &state)
+{
+    const char *test = "123456789";
+    for(auto _ : state)
+    {
+        benchmark::DoNotOptimize(crc16(test, strlen(test), 0));
+    }
+}
+BENCHMARK(bm_crc16_standard_test_vector);
+
+static void bm_crc16_large_text(benchmark::State &state)
+{
+    const char *big_data =
+        "The class everyone had really been looking forward to was Defence "
+        "Against the Dark Arts, but Quirrell's lessons turned out to be a bit "
+        "of a joke.His classroom smelled strongly of garlic, which everyone "
+        "said was to ward off a vampire he'd met in Romania, and was afraid "
+        "would be coming back to get him one of these days.His turban, he told "
+        "them, had been given to him by an African prince as a thank-you for "
+        "getting rid of a troublesome zombie, but they weren't sure they "
+        "believed this story.For one thing, when Seamus Finnegan asked eagerly "
+        "to hear how Quirrell had fought off the zombie, Quirrell went pink "
+        "and sta";
+    for(auto _ : state)
+    {
+        benchmark::DoNotOptimize(crc16(big_data, strlen(big_data), 0));
+    }
+}
+BENCHMARK(bm_crc16_large_text);
+
+static void bm_crc16_incremental(benchmark::State &state)
+{
+    const char *part1 = "Hello, ";
+    const char *part2 = "World!";
+    for(auto _ : state)
+    {
+        uint16_t crc1 = crc16(part1, strlen(part1), 0);
+        benchmark::DoNotOptimize(crc16(part2, strlen(part2), crc1));
+    }
+}
+BENCHMARK(bm_crc16_incremental);
+
+static void bm_crc16_edge_cases(benchmark::State &state)
+{
+    const char *data = "test";
+    for(auto _ : state)
+    {
+        benchmark::DoNotOptimize(crc16(nullptr, 0, 0));
+        benchmark::DoNotOptimize(crc16(nullptr, 10, 123));
+        benchmark::DoNotOptimize(crc16(data, 0, 0));
+    }
+}
+BENCHMARK(bm_crc16_edge_cases);
+
+static void bm_crc16_consistency(benchmark::State &state)
+{
+    const char *test_data = "The quick brown fox jumps over the lazy dog";
+    for(auto _ : state)
+    {
+        benchmark::DoNotOptimize(crc16(test_data, strlen(test_data), 0));
+    }
+}
+BENCHMARK(bm_crc16_consistency);
+
+static void bm_crc16_high_concurrency(benchmark::State &state)
+{
+    const std::string base = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for(auto _ : state)
+    {
+        for(int i = 0; i < 1000; ++i)
+        {
+            std::string s = base + std::to_string(i);
+            benchmark::DoNotOptimize(crc16(s.c_str(), s.size(), 0));
+        }
+    }
+}
+BENCHMARK(bm_crc16_high_concurrency);

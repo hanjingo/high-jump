7b09f52466a3ff126b2dfa1a09ae6752128ce7b3
hanjingo <hehehunanchina@live.com>
2026-08-11 16:20:42 +0800

    upgrade db_conn_pool

--- hj/db/db_conn_pool.hpp
+++ hj/db/db_conn_pool.hpp
@@ -19,156 +19,327 @@
 #ifndef DB_CONN_POOL_HPP
 #define DB_CONN_POOL_HPP
 
-// NOTIC: This file is written in C++11 by myself.
+// NOTIC: This file is written in C++17 by myself.
 //  I am not sure if it is performing well and stable.
 //  If you need an industrial level code, please use the following libraries:
 //      1.https://github.com/SOCI/soci/blob/master/include/soci/connection-pool.h
 
 #include <queue>
 #include <memory>
+#include <thread>
 #include <mutex>
-#include <atomic>
 #include <condition_variable>
 #include <chrono>
 #include <functional>
-#include <stdexcept>
-#include <cassert>
-#include <optional>
+#include <utility>
+#include <atomic>
 
 namespace hj
 {
 
 template <typename Conn>
-class db_conn_pool
+class db_conn_pool : public std::enable_shared_from_this<db_conn_pool<Conn>>
 {
-  public:
-    using conn_ptr_t   = std::shared_ptr<Conn>;
-    using make_conn_t  = std::function<conn_ptr_t()>;
-    using check_conn_t = std::function<bool(conn_ptr_t)>;
-
-  public:
-    explicit db_conn_pool(const std::size_t capa, make_conn_t &&make)
-        : _pool{}
-        , _cond{}
-        , _mu{}
-        , _closed{false}
-        , _capa{capa}
-        , _make{std::move(make)}
-        , _check{[](conn_ptr_t) -> bool { return true; }}
+  private:
+    struct private_tag
     {
-        for(std::size_t i = 0; i < capa; ++i)
-            _pool.push(_make());
-    }
+        explicit private_tag() = default;
+    };
 
-    explicit db_conn_pool(const std::size_t capa,
-                          make_conn_t     &&make,
-                          check_conn_t    &&check)
-        : _pool{}
-        , _cond{}
-        , _mu{}
-        , _closed{false}
-        , _capa{capa}
+  public:
+    using conn_ptr_t    = std::shared_ptr<Conn>;
+    using deleter_t     = std::function<void(Conn *)>;
+    using conn_handle_t = std::unique_ptr<Conn, deleter_t>;
+    using make_conn_t   = std::function<conn_ptr_t()>;
+    using check_conn_t  = std::function<bool(conn_ptr_t)>;
+
+  public:
+    explicit db_conn_pool(private_tag,
+                          std::size_t    capa,
+                          std::size_t    min_size,
+                          make_conn_t  &&make,
+                          check_conn_t &&check)
+        : _capa{capa}
+        , _min_size{(min_size > capa) ? capa : min_size}
         , _make{std::move(make)}
         , _check{std::move(check)}
+        , _closed{false}
+        , _total_cnt{0}
     {
-        for(std::size_t i = 0; i < capa; ++i)
-            _pool.push(_make());
     }
 
     ~db_conn_pool() { close(); }
 
-    inline bool        is_closed() const { return _closed; }
+    static std::shared_ptr<db_conn_pool> create(std::size_t  capa,
+                                                std::size_t  min_size,
+                                                make_conn_t  make,
+                                                check_conn_t check = nullptr)
+    {
+        auto pool = std::make_shared<db_conn_pool>(private_tag{},
+                                                   capa,
+                                                   min_size,
+                                                   std::move(make),
+                                                   std::move(check));
+        pool->_init();
+        return pool;
+    }
+
+    inline bool is_closed() const
+    {
+        std::lock_guard<std::mutex> lock(_mu);
+        return _closed.load();
+    }
+
+    inline bool is_empty() const
+    {
+        std::lock_guard<std::mutex> lock(_mu);
+        return _pool.empty();
+    }
+
     inline std::size_t capa() const { return _capa; }
-    inline std::size_t size() const
+    inline std::size_t min_size() const { return _min_size; }
+
+    inline std::size_t idle() const
     {
         std::lock_guard<std::mutex> lock(_mu);
         return _pool.size();
     }
 
-    conn_ptr_t acquire(const int timeout_ms = 0)
+    inline std::size_t active() const
     {
-        std::unique_lock<std::mutex> lock{_mu};
-        if(_closed)
-            return nullptr;
+        std::lock_guard<std::mutex> lock(_mu);
+        return _total_cnt - _pool.size();
+    }
 
-        if(timeout_ms > 0)
+    inline std::size_t total() const
+    {
+        std::lock_guard<std::mutex> lock(_mu);
+        return _total_cnt;
+    }
+
+    conn_handle_t acquire(int timeout_ms = -1)
+    {
+        auto start_time   = std::chrono::steady_clock::now();
+        int  remaining_ms = timeout_ms;
+
+        while(true)
         {
-            // wait until pool is non-empty or pool is closed
-            if(!_cond.wait_for(
-                   lock,
-                   std::chrono::milliseconds(timeout_ms),
-                   [this] { return !this->_pool.empty() || this->_closed; }))
-                return nullptr;
+            conn_ptr_t conn            = nullptr;
+            bool       need_create_new = false;
 
-            if(_closed)
-                return nullptr;
-        } else
-        {
-            if(_pool.empty())
-                return nullptr;
-        }
-
-        while(!_pool.empty())
-        {
-            if(_closed)
-                return nullptr;
-
-            conn_ptr_t conn_ptr = _pool.front();
-            _pool.pop();
-
-            if(_closed)
-                return nullptr;
-
-            if(!_check(conn_ptr))
             {
-                // replace bad connection with a new one (only if not closed)
-                if(!_closed)
-                    _pool.push(_make());
+                std::unique_lock<std::mutex> lock(_mu);
+                auto                         wait_cond = [this] {
+                    return !_pool.empty() || _closed.load()
+                           || _total_cnt < _capa;
+                };
 
+                if(remaining_ms < 0)
+                {
+                    _cond.wait(lock, wait_cond);
+                } else if(remaining_ms > 0)
+                {
+                    if(!_cond.wait_for(lock,
+                                       std::chrono::milliseconds(remaining_ms),
+                                       wait_cond))
+                        return nullptr;
+                } else
+                {
+                    if(_pool.empty() && _total_cnt >= _capa)
+                        return nullptr;
+                }
+
+                if(_closed.load())
+                    return nullptr;
+
+                if(!_pool.empty())
+                {
+                    conn = std::move(_pool.front());
+                    _pool.pop();
+                } else if(_total_cnt < _capa)
+                {
+                    _total_cnt++;
+                    need_create_new = true;
+                } else
+                {
+                    return nullptr;
+                }
+            }
+
+            if(need_create_new)
+            {
+                try
+                {
+                    conn = _make();
+                }
+                catch(...)
+                {
+                    conn = nullptr;
+                }
+
+                if(!conn)
+                {
+                    {
+                        std::lock_guard<std::mutex> lock(_mu);
+                        _total_cnt--;
+                    }
+                    _cond.notify_one();
+
+                    if(timeout_ms >= 0)
+                    {
+                        auto elapsed =
+                            std::chrono::duration_cast<
+                                std::chrono::milliseconds>(
+                                std::chrono::steady_clock::now() - start_time)
+                                .count();
+                        remaining_ms = timeout_ms - static_cast<int>(elapsed);
+                        if(remaining_ms <= 0)
+                            return nullptr;
+                    }
+                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                    continue;
+                }
+            }
+
+            bool valid = false;
+            if(_check && conn)
+            {
+                try
+                {
+                    valid = _check(conn);
+                }
+                catch(...)
+                {
+                    valid = false;
+                }
+            } else
+            {
+                valid = (conn != nullptr);
+            }
+
+            if(!valid)
+            {
+                {
+                    std::lock_guard<std::mutex> lock(_mu);
+                    _total_cnt--;
+                }
+                _cond.notify_one();
+
+                if(timeout_ms >= 0)
+                {
+                    auto elapsed =
+                        std::chrono::duration_cast<std::chrono::milliseconds>(
+                            std::chrono::steady_clock::now() - start_time)
+                            .count();
+                    remaining_ms = timeout_ms - static_cast<int>(elapsed);
+                    if(remaining_ms <= 0)
+                        return nullptr;
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
                 continue;
             }
 
-            // auto give_back
-            return conn_ptr_t(conn_ptr.get(), [this, conn_ptr](Conn *) {
-                this->_release(conn_ptr);
-            });
-        }
+            {
+                std::lock_guard<std::mutex> lock(_mu);
+                if(_closed.load())
+                {
+                    if(_total_cnt > 0)
+                        _total_cnt--;
 
-        return nullptr;
+                    return nullptr;
+                }
+            }
+
+            std::weak_ptr<db_conn_pool> weak_self = this->shared_from_this();
+            return conn_handle_t(
+                conn.get(),
+                [weak_self, internal_conn = conn](Conn *) mutable {
+                    if(auto self = weak_self.lock())
+                        self->_release(std::move(internal_conn));
+                });
+        }
     }
 
     void close()
     {
-        std::lock_guard<std::mutex> lock{_mu};
-        _closed = true;
-        while(!_pool.empty())
-            _pool.pop();
+        std::queue<conn_ptr_t> to_destroy;
+        {
+            bool expected = false;
+            if(!_closed.compare_exchange_strong(expected, true))
+                return;
 
+            std::lock_guard<std::mutex> lock(_mu);
+            _total_cnt -= _pool.size();
+            _pool.swap(to_destroy);
+        }
         _cond.notify_all();
     }
 
   private:
+    void _init()
+    {
+        for(std::size_t i = 0; i < _min_size; ++i)
+        {
+            try
+            {
+                auto conn = _make();
+                if(conn)
+                {
+                    std::lock_guard<std::mutex> lock(_mu);
+                    _pool.push(std::move(conn));
+                    _total_cnt++;
+                }
+            }
+            catch(...)
+            {
+            }
+        }
+    }
+
     void _release(conn_ptr_t conn)
     {
-        std::lock_guard<std::mutex> lock{_mu};
-        if(!_closed)
+        if(!conn)
+            return;
+
+        conn_ptr_t to_destroy;
         {
-            _pool.push(conn);
-            _cond.notify_one();
+            std::lock_guard<std::mutex> lock{_mu};
+            if(!_closed.load())
+            {
+                if(_pool.size() < _min_size)
+                {
+                    _pool.push(std::move(conn));
+                    _cond.notify_one();
+                    return;
+                }
+
+                _total_cnt--;
+                _cond.notify_one();
+                to_destroy = std::move(conn);
+                return;
+            }
+
+            if(_total_cnt > 0)
+                _total_cnt--;
+
+            to_destroy = std::move(conn);
         }
     }
 
   private:
+    mutable std::mutex      _mu;
     std::queue<conn_ptr_t>  _pool;
     std::condition_variable _cond;
-    mutable std::mutex      _mu;
-    std::atomic<bool>       _closed;
-    std::size_t             _capa;
+
+    const std::size_t        _capa;
+    const std::size_t        _min_size;
+    std::atomic<bool>        _closed{false};
+    std::atomic<std::size_t> _total_cnt{0};
 
     make_conn_t  _make;
     check_conn_t _check;
 };
 
-};
+} // namespace hj
 
 #endif
--- tests/db_conn_pool_test.cpp
+++ tests/db_conn_pool_test.cpp
@@ -1,232 +1,336 @@
-
 #include <gtest/gtest.h>
 #include <hj/db/db_conn_pool.hpp>
-#include <hj/db/sqlite.hpp>
+#include <sqlite3.h>
 #include <thread>
 #include <vector>
+#include <atomic>
+#include <chrono>
+#include <stdexcept>
 
-TEST(db_conn_pool, capa)
+namespace
 {
-    hj::db_conn_pool<sqlite3> pool{
-        5,
-        []() -> hj::db_conn_pool<sqlite3>::conn_ptr_t {
-            sqlite3 *db;
-            int      ret = sqlite3_open("007.db", &db);
-            if(ret != SQLITE_OK)
-            {
-                std::cout << "open db fail with err = " << sqlite3_errmsg(db)
-                          << std::endl;
-                return nullptr;
-            }
-            return std::shared_ptr<sqlite3>(db, [](sqlite3 *db) {
-                sqlite3_close(db);
-            });
-        }};
-    ASSERT_EQ(pool.capa(), 5);
+hj::db_conn_pool<sqlite3>::conn_ptr_t make_sqlite_mem_conn()
+{
+    sqlite3 *db = nullptr;
+    if(sqlite3_open(":memory:", &db) != SQLITE_OK)
+    {
+        if(db)
+            sqlite3_close(db);
+        return nullptr;
+    }
+    return std::shared_ptr<sqlite3>(db, [](sqlite3 *d) {
+        if(d)
+            sqlite3_close(d);
+    });
 }
 
-TEST(db_conn_pool, acquire)
+struct MockConn
 {
-    hj::db_conn_pool<sqlite3> pool{
-        5,
-        []() -> hj::db_conn_pool<sqlite3>::conn_ptr_t {
-            sqlite3 *db;
-            int      ret = sqlite3_open("007.db", &db);
-            if(ret != SQLITE_OK)
-            {
-                std::cout << "open db fail with err = " << sqlite3_errmsg(db)
-                          << std::endl;
-                return nullptr;
-            }
-            return std::shared_ptr<sqlite3>(db, [](sqlite3 *db) {
-                sqlite3_close(db);
-            });
-        },
-        [](hj::db_conn_pool<sqlite3>::conn_ptr_t conn) -> bool {
-            if(conn == nullptr)
-                return false;
-            return true;
-        }};
-    auto conn = pool.acquire();
-    ASSERT_TRUE(conn != nullptr);
-    auto conn1 = pool.acquire();
-    ASSERT_TRUE(conn1 != nullptr);
-    auto conn2 = pool.acquire();
-    ASSERT_TRUE(conn2 != nullptr);
-    auto conn3 = pool.acquire();
-    ASSERT_TRUE(conn3 != nullptr);
-    auto conn4 = pool.acquire();
-    ASSERT_TRUE(conn4 != nullptr);
-    auto conn5 = pool.acquire();
-    ASSERT_TRUE(conn5 == nullptr);
+    int  id{0};
+    bool is_valid{true};
+};
+} // namespace
+
+TEST(db_conn_pool, basic_capacity_and_statistics)
+{
+    constexpr std::size_t CAPA     = 5;
+    constexpr std::size_t MIN_SIZE = 2;
+
+    auto pool =
+        hj::db_conn_pool<sqlite3>::create(CAPA, MIN_SIZE, make_sqlite_mem_conn);
+    ASSERT_NE(pool, nullptr);
+
+    EXPECT_EQ(pool->capa(), CAPA);
+    EXPECT_EQ(pool->min_size(), MIN_SIZE);
+    EXPECT_EQ(pool->idle(), MIN_SIZE);
+    EXPECT_EQ(pool->active(), 0);
+    EXPECT_EQ(pool->total(), MIN_SIZE);
+    EXPECT_FALSE(pool->is_empty());
+    EXPECT_FALSE(pool->is_closed());
 }
 
-TEST(db_conn_pool, release_and_reuse)
+TEST(db_conn_pool, min_size_clamping_boundary)
 {
-    hj::db_conn_pool<sqlite3> pool{
-        2,
-        []() -> hj::db_conn_pool<sqlite3>::conn_ptr_t {
-            sqlite3 *db;
-            int      ret = sqlite3_open("008.db", &db);
-            if(ret != SQLITE_OK)
-                return nullptr;
-            return std::shared_ptr<sqlite3>(db, [](sqlite3 *db) {
-                sqlite3_close(db);
-            });
-        }};
-    auto conn1 = pool.acquire();
-    auto conn2 = pool.acquire();
-    ASSERT_EQ(pool.size(), 0);
-    ASSERT_TRUE(conn1 != nullptr);
-    ASSERT_TRUE(conn2 != nullptr);
+    // 故意传入 min_size(10) > capa(3)
+    auto pool = hj::db_conn_pool<sqlite3>::create(3, 10, make_sqlite_mem_conn);
+    ASSERT_NE(pool, nullptr);
+
+    EXPECT_EQ(pool->capa(), 3);
+    EXPECT_EQ(pool->min_size(), 3); // 应该被截断为 3
+    EXPECT_EQ(pool->idle(), 3);
+    EXPECT_EQ(pool->total(), 3);
+}
+
+TEST(db_conn_pool, acquire_release_and_min_size_recycling)
+{
+    constexpr std::size_t CAPA     = 4;
+    constexpr std::size_t MIN_SIZE = 2;
+
+    auto pool =
+        hj::db_conn_pool<sqlite3>::create(CAPA, MIN_SIZE, make_sqlite_mem_conn);
+
+    EXPECT_EQ(pool->idle(), 2);
+    EXPECT_EQ(pool->active(), 0);
+    EXPECT_EQ(pool->total(), 2);
+
+    auto conn1 = pool->acquire();
+    ASSERT_NE(conn1, nullptr);
+    EXPECT_EQ(pool->idle(), 1);
+    EXPECT_EQ(pool->active(), 1);
+    EXPECT_EQ(pool->total(), 2);
+
+    auto conn2 = pool->acquire();
+    ASSERT_NE(conn2, nullptr);
+    EXPECT_EQ(pool->idle(), 0);
+    EXPECT_EQ(pool->active(), 2);
+    EXPECT_EQ(pool->total(), 2);
+    EXPECT_TRUE(pool->is_empty());
+
+    auto conn3 = pool->acquire();
+    ASSERT_NE(conn3, nullptr);
+    EXPECT_EQ(pool->idle(), 0);
+    EXPECT_EQ(pool->active(), 3);
+    EXPECT_EQ(pool->total(), 3);
+
+    auto conn4 = pool->acquire();
+    ASSERT_NE(conn4, nullptr);
+    EXPECT_EQ(pool->idle(), 0);
+    EXPECT_EQ(pool->active(), 4);
+    EXPECT_EQ(pool->total(), 4);
+
+    auto conn_fail = pool->acquire(0);
+    EXPECT_EQ(conn_fail, nullptr);
 
     conn1.reset();
-    ASSERT_EQ(pool.size(), 1);
-    auto conn3 = pool.acquire();
-    ASSERT_TRUE(conn3 != nullptr);
+    EXPECT_EQ(pool->idle(), 1);
+    EXPECT_EQ(pool->active(), 3);
+    EXPECT_EQ(pool->total(), 4);
+
     conn2.reset();
+    EXPECT_EQ(pool->idle(), 2);
+    EXPECT_EQ(pool->active(), 2);
+    EXPECT_EQ(pool->total(), 4);
+
     conn3.reset();
-    ASSERT_EQ(pool.size(), 2);
+    EXPECT_EQ(pool->idle(), 2);
+    EXPECT_EQ(pool->active(), 1);
+    EXPECT_EQ(pool->total(), 3);
+
+    conn4.reset();
+    EXPECT_EQ(pool->idle(), 2);
+    EXPECT_EQ(pool->active(), 0);
+    EXPECT_EQ(pool->total(), 2);
 }
 
-TEST(db_conn_pool, timeout)
+TEST(db_conn_pool, non_blocking_acquire)
 {
-    hj::db_conn_pool<sqlite3> pool{
-        1,
-        []() -> hj::db_conn_pool<sqlite3>::conn_ptr_t {
-            sqlite3 *db;
-            int      ret = sqlite3_open("009.db", &db);
-            if(ret != SQLITE_OK)
-                return nullptr;
-            return std::shared_ptr<sqlite3>(db, [](sqlite3 *db) {
-                sqlite3_close(db);
-            });
-        }};
-    auto conn1 = pool.acquire();
-    ASSERT_TRUE(conn1 != nullptr);
+    auto pool = hj::db_conn_pool<sqlite3>::create(1, 1, make_sqlite_mem_conn);
 
-    auto conn2 = pool.acquire(100);
-    ASSERT_TRUE(conn2 == nullptr);
+    auto conn1 = pool->acquire(0);
+    ASSERT_NE(conn1, nullptr);
+
+    auto start   = std::chrono::steady_clock::now();
+    auto conn2   = pool->acquire(0);
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+                       std::chrono::steady_clock::now() - start)
+                       .count();
+
+    EXPECT_EQ(conn2, nullptr);
+    EXPECT_LT(elapsed, 20);
+}
+
+TEST(db_conn_pool, timeout_acquire)
+{
+    auto pool = hj::db_conn_pool<sqlite3>::create(1, 1, make_sqlite_mem_conn);
+
+    auto conn1 = pool->acquire();
+    ASSERT_NE(conn1, nullptr);
+
+    auto start   = std::chrono::steady_clock::now();
+    auto conn2   = pool->acquire(100);
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
+                       std::chrono::steady_clock::now() - start)
+                       .count();
+
+    EXPECT_EQ(conn2, nullptr);
+    EXPECT_GE(elapsed, 90);
+}
+
+TEST(db_conn_pool, infinite_wait_acquire)
+{
+    auto pool = hj::db_conn_pool<sqlite3>::create(1, 1, make_sqlite_mem_conn);
+
+    auto conn1 = pool->acquire();
+    ASSERT_NE(conn1, nullptr);
+
+    std::atomic<bool>                        acquired{false};
+    hj::db_conn_pool<sqlite3>::conn_handle_t c;
+    std::thread                              t([&]() {
+        c = pool->acquire(-1);
+        if(c)
+            acquired = true;
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    EXPECT_FALSE(acquired.load());
+
     conn1.reset();
+    t.join();
+
+    EXPECT_TRUE(acquired.load());
+    EXPECT_EQ(pool->idle(), 0);
+    EXPECT_EQ(pool->active(), 1);
 }
 
-TEST(db_conn_pool, check_conn)
+TEST(db_conn_pool, health_check_failure_and_recovery)
 {
-    int                       fail_count = 0;
-    hj::db_conn_pool<sqlite3> pool{
+    int  make_counter = 0;
+    auto pool         = hj::db_conn_pool<MockConn>::create(
         2,
-        []() -> hj::db_conn_pool<sqlite3>::conn_ptr_t {
-            sqlite3 *db;
-            int      ret = sqlite3_open("010.db", &db);
-            if(ret != SQLITE_OK)
-                return nullptr;
-            return std::shared_ptr<sqlite3>(db, [](sqlite3 *db) {
-                sqlite3_close(db);
-            });
+        1,
+        [&make_counter]() {
+            auto conn = std::make_shared<MockConn>();
+            conn->id  = ++make_counter;
+            return conn;
         },
-        [&fail_count](hj::db_conn_pool<sqlite3>::conn_ptr_t conn) -> bool {
-            if(conn == nullptr)
-            {
-                ++fail_count;
-                return false;
-            }
-            return true;
-        }};
-    auto conn1 = pool.acquire();
-    auto conn2 = pool.acquire();
-    ASSERT_TRUE(conn1 != nullptr);
-    ASSERT_TRUE(conn2 != nullptr);
+        [](std::shared_ptr<MockConn> conn) { return conn && conn->is_valid; });
+
+    auto conn1 = pool->acquire();
+    ASSERT_NE(conn1, nullptr);
+    EXPECT_EQ(conn1->id, 1);
+
+    conn1->is_valid = false;
     conn1.reset();
-    conn2.reset();
-    ASSERT_EQ(fail_count, 0);
+
+    auto conn2 = pool->acquire();
+    ASSERT_NE(conn2, nullptr);
+    EXPECT_TRUE(conn2->is_valid);
+    EXPECT_GT(conn2->id, 1);
+    EXPECT_EQ(pool->total(), 1);
 }
 
-TEST(db_conn_pool, concurrent_acquire_release)
+TEST(db_conn_pool, health_check_throws_exception)
 {
-    hj::db_conn_pool<sqlite3> pool{
-        3,
-        []() -> hj::db_conn_pool<sqlite3>::conn_ptr_t {
-            sqlite3 *db;
-            int      ret = sqlite3_open("011.db", &db);
-            if(ret != SQLITE_OK)
-                return nullptr;
-            return std::shared_ptr<sqlite3>(db, [](sqlite3 *db) {
-                sqlite3_close(db);
-            });
-        }};
-    std::atomic<int>         success{0};
+    auto pool = hj::db_conn_pool<MockConn>::create(
+        1,
+        0,
+        []() { return std::make_shared<MockConn>(); },
+        [](std::shared_ptr<MockConn>) -> bool {
+            throw std::runtime_error("Check Exception");
+        });
+
+    auto conn = pool->acquire(50);
+    EXPECT_EQ(conn, nullptr);
+    EXPECT_EQ(pool->total(), 0);
+}
+
+TEST(db_conn_pool, make_factory_throws_or_returns_null)
+{
+    std::atomic<bool> should_fail{true};
+
+    auto pool = hj::db_conn_pool<MockConn>::create(
+        2,
+        0,
+        [&should_fail]() -> std::shared_ptr<MockConn> {
+            if(should_fail.load())
+            {
+                throw std::runtime_error("DB Connection Failed");
+            }
+            return std::make_shared<MockConn>();
+        });
+
+    EXPECT_EQ(pool->idle(), 0);
+    EXPECT_EQ(pool->total(), 0);
+
+    auto conn1 = pool->acquire(50);
+    EXPECT_EQ(conn1, nullptr);
+    EXPECT_EQ(pool->total(), 0);
+
+    should_fail = false;
+
+    auto conn2 = pool->acquire(100);
+    ASSERT_NE(conn2, nullptr);
+    EXPECT_EQ(pool->total(), 1);
+    EXPECT_EQ(pool->active(), 1);
+}
+
+TEST(db_conn_pool, pool_destruction_before_handle_release)
+{
+    hj::db_conn_pool<sqlite3>::conn_handle_t external_conn;
+
+    {
+        auto pool =
+            hj::db_conn_pool<sqlite3>::create(1, 1, make_sqlite_mem_conn);
+        external_conn = pool->acquire();
+        ASSERT_NE(external_conn, nullptr);
+    }
+
+    EXPECT_NO_THROW(external_conn.reset());
+}
+
+TEST(db_conn_pool, close_idempotency_and_acquire_refusal)
+{
+    auto pool = hj::db_conn_pool<sqlite3>::create(2, 2, make_sqlite_mem_conn);
+
+    auto conn1 = pool->acquire();
+    ASSERT_NE(conn1, nullptr);
+    EXPECT_EQ(pool->total(), 2);
+    EXPECT_EQ(pool->idle(), 1);
+
+    pool->close();
+    EXPECT_TRUE(pool->is_closed());
+    EXPECT_EQ(pool->idle(), 0);
+    EXPECT_EQ(pool->total(), 1);
+
+    EXPECT_NO_THROW(pool->close());
+
+    auto conn2 = pool->acquire();
+    EXPECT_EQ(conn2, nullptr);
+
+    conn1.reset();
+    EXPECT_EQ(pool->total(), 0);
+    EXPECT_EQ(pool->active(), 0);
+    EXPECT_EQ(pool->idle(), 0);
+}
+
+TEST(db_conn_pool, high_concurrency_acquire_release)
+{
+    constexpr std::size_t CAPA           = 5;
+    constexpr std::size_t MIN_SIZE       = 2;
+    constexpr int         THREAD_COUNT   = 10;
+    constexpr int         OPS_PER_THREAD = 30;
+
+    auto pool =
+        hj::db_conn_pool<sqlite3>::create(CAPA, MIN_SIZE, make_sqlite_mem_conn);
+
+    std::atomic<int>         successful_acquires{0};
+    std::atomic<int>         failed_acquires{0};
     std::vector<std::thread> threads;
-    for(int i = 0; i < 10; ++i)
+
+    for(int i = 0; i < THREAD_COUNT; ++i)
     {
         threads.emplace_back([&]() {
-            auto conn = pool.acquire(200);
-            if(conn)
+            for(int j = 0; j < OPS_PER_THREAD; ++j)
             {
-                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                ++success;
+                auto conn = pool->acquire(20);
+                if(conn)
+                {
+                    ++successful_acquires;
+                    std::this_thread::sleep_for(std::chrono::microseconds(100));
+                } else
+                {
+                    ++failed_acquires;
+                }
             }
         });
     }
+
     for(auto &t : threads)
         t.join();
-    ASSERT_EQ(pool.size(), 3);
-    ASSERT_GE(success, 3);
-}
 
-TEST(db_conn_pool, close_and_acquire)
-{
-    hj::db_conn_pool<sqlite3> pool{
-        2,
-        []() -> hj::db_conn_pool<sqlite3>::conn_ptr_t {
-            sqlite3 *db;
-            int      ret = sqlite3_open("012.db", &db);
-            if(ret != SQLITE_OK)
-                return nullptr;
-            return std::shared_ptr<sqlite3>(db, [](sqlite3 *db) {
-                sqlite3_close(db);
-            });
-        }};
-    auto conn1 = pool.acquire();
-    pool.close();
-    ASSERT_TRUE(pool.is_closed());
-    auto conn2 = pool.acquire();
-    ASSERT_EQ(conn2, nullptr);
-    conn1.reset();
-    ASSERT_EQ(pool.size(), 0);
-}
+    EXPECT_GT(successful_acquires.load(), 0);
 
-TEST(db_conn_pool, acquire_after_close_multithread)
-{
-    hj::db_conn_pool<sqlite3> pool{
-        1,
-        []() -> hj::db_conn_pool<sqlite3>::conn_ptr_t {
-            sqlite3 *db;
-            int      ret = sqlite3_open("013.db", &db);
-            if(ret != SQLITE_OK)
-                return nullptr;
-            return std::shared_ptr<sqlite3>(db, [](sqlite3 *db) {
-                sqlite3_close(db);
-            });
-        }};
-    std::atomic<int> null_count{0};
-    std::thread      t1([&]() {
-        auto c = pool.acquire();
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        c.reset();
-    });
-    std::thread      t2([&]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        pool.close();
-    });
-    std::thread      t3([&]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(20));
-        auto c = pool.acquire(100);
-        if(!c)
-            ++null_count;
-    });
-    t1.join();
-    t2.join();
-    t3.join();
-    ASSERT_TRUE(pool.is_closed());
-    ASSERT_EQ(null_count, 1);
+    EXPECT_EQ(pool->active(), 0);
+    EXPECT_EQ(pool->idle(), MIN_SIZE);
+    EXPECT_EQ(pool->total(), MIN_SIZE);
 }

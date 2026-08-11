65c584cb8c079fb57d86386ccad05b7d994cae57
hanjingo <hehehunanchina@live.com>
2026-08-11 17:41:50 +0800

    upgrade sqlite

--- hj/db/sqlite.hpp
+++ hj/db/sqlite.hpp
@@ -1,6 +1,6 @@
 /*
  *  This file is part of high-jump(hj).
- *  Copyright (C) 2025 hanjingo <hehehunanchina@live.com>
+ *  Copyright (C) 2026 hanjingo <hehehunanchina@live.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -15,29 +15,53 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef SQLITE_HPP
 #define SQLITE_HPP
 
+#include "db_conn.hpp"
+
 #include <sqlite3.h>
-#include <string>
-#include <vector>
-#include <stdexcept>
+#include <cstdarg>
 #include <cstring>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <system_error>
+#include <type_traits>
+#include <utility>
+#include <vector>
 
 namespace hj
 {
 
-class sqlite
+class sqlite : public db_conn
 {
   public:
-    typedef int (*exec_cb)(void *in, int argc, char **argv, char **col_name);
+    sqlite() = default;
 
-  public:
-    sqlite()
-        : _db{nullptr}
+    explicit sqlite(const std::string &filename) { open(filename); }
+
+    ~sqlite() override { close(); }
+
+    sqlite(sqlite &&other) noexcept
+        : _db(std::exchange(other._db, nullptr))
+        , _db_path(std::move(other._db_path))
+        , _last_err(std::move(other._last_err))
     {
     }
-    ~sqlite() { close(); }
+
+    sqlite &operator=(sqlite &&other) noexcept
+    {
+        if(this != &other)
+        {
+            close();
+            _db       = std::exchange(other._db, nullptr);
+            _db_path  = std::move(other._db_path);
+            _last_err = std::move(other._last_err);
+        }
+        return *this;
+    }
 
     static std::string mprintf(const char *format, ...)
     {
@@ -58,10 +82,20 @@ class sqlite
         close();
         _last_err.clear();
         if(sqlite3_open(filename.c_str(), &_db) == SQLITE_OK)
+        {
+            _db_path = filename;
             return true;
+        }
 
-        _last_err = sqlite3_errmsg(_db ? _db : nullptr);
-        close();
+        if(_db)
+        {
+            _last_err = sqlite3_errmsg(_db);
+            sqlite3_close_v2(_db);
+            _db = nullptr;
+        } else
+        {
+            _last_err = "Failed to allocate sqlite memory";
+        }
         return false;
     }
 
@@ -70,106 +104,224 @@ class sqlite
         if(!_db)
             return;
 
-        sqlite3_close(_db);
+        sqlite3_close_v2(_db);
         _db = nullptr;
     }
 
     inline bool        is_open() const { return _db != nullptr; }
-    inline std::string get_last_error() { return _last_err; }
-    inline bool        begin() { return exec("BEGIN TRANSACTION;"); }
-    inline bool        commit() { return exec("COMMIT;"); }
-    inline bool        rollback() { return exec("ROLLBACK;"); }
+    inline std::string get_last_error() const { return _last_err; }
+    inline std::string path() const { return _db_path; }
 
-    bool exec(const std::string &sql, exec_cb callback = nullptr)
+  protected:
+    err_t begin_impl() override
     {
+        auto res = exec("BEGIN TRANSACTION;");
+        return res.ec;
+    }
+
+    err_t commit_impl() override
+    {
+        auto res = exec("COMMIT;");
+        return res.ec;
+    }
+
+    err_t rollback_impl() override
+    {
+        auto res = exec("ROLLBACK;");
+        return res.ec;
+    }
+
+    exec_result exec_impl(std::string_view sql,
+                          const val_in_t  *args,
+                          std::size_t      count) override
+    {
+        exec_result res{};
         if(!_db)
-            return false;
-
-        char *errmsg = nullptr;
-        if(sqlite3_exec(_db, sql.c_str(), callback, nullptr, &errmsg)
-           == SQLITE_OK)
-            return true;
-
-        if(errmsg)
         {
-            _last_err.assign(errmsg, strlen(errmsg));
-            sqlite3_free(errmsg);
+            res.ec = std::make_error_code(std::errc::not_connected);
+            return res;
+        }
+
+        sqlite3_stmt *stmt = nullptr;
+        int           rc   = sqlite3_prepare_v2(_db,
+                                                sql.data(),
+                                                static_cast<int>(sql.size()),
+                                                &stmt,
+                                                nullptr);
+        if(rc != SQLITE_OK)
+        {
+            _last_err = sqlite3_errmsg(_db);
+            res.ec    = std::make_error_code(std::errc::bad_message);
+            return res;
+        }
+
+        if(!_bind_stmt_args(stmt, args, count))
+        {
+            res.ec = std::make_error_code(std::errc::invalid_argument);
+            sqlite3_finalize(stmt);
+            return res;
+        }
+
+        rc = sqlite3_step(stmt);
+        if(rc == SQLITE_DONE || rc == SQLITE_ROW)
+        {
+            res.ec             = err_t();
+            res.affected_rows  = sqlite3_changes(_db);
+            res.last_insert_id = sqlite3_last_insert_rowid(_db);
         } else
         {
             _last_err = sqlite3_errmsg(_db);
-        }
-        return false;
-    }
-
-    // Query: returns vector of rows, each row is vector<string>
-    std::vector<std::vector<std::string>> query(const std::string &sql)
-    {
-        std::vector<std::vector<std::string>> rows;
-        if(!_db)
-            return rows;
-
-        sqlite3_stmt *stmt = nullptr;
-        if(sqlite3_prepare_v2(_db, sql.c_str(), -1, &stmt, nullptr)
-           != SQLITE_OK)
-        {
-            _last_err = sqlite3_errmsg(_db);
-            return rows;
+            res.ec    = std::make_error_code(std::errc::io_error);
         }
 
-        int cols = sqlite3_column_count(stmt);
-        while(sqlite3_step(stmt) == SQLITE_ROW)
-        {
-            std::vector<std::string> row;
-            for(int i = 0; i < cols; ++i)
-            {
-                const char *val = reinterpret_cast<const char *>(
-                    sqlite3_column_text(stmt, i));
-                row.push_back(val ? val : "");
-            }
-            rows.push_back(std::move(row));
-        }
         sqlite3_finalize(stmt);
-        return rows;
+        return res;
     }
 
-    // get the sequence of the last inserted row
-    int64_t sequence(const std::string &table)
+    err_t query_impl(ret_t           &outs,
+                     std::string_view sql,
+                     const val_in_t  *args,
+                     std::size_t      count) override
     {
         if(!_db)
-            return -1;
+            return std::make_error_code(std::errc::not_connected);
 
-        std::string sql = "SELECT seq FROM sqlite_sequence WHERE name='" + table
-                          + "' LIMIT 1;";
         sqlite3_stmt *stmt = nullptr;
-        if(sqlite3_prepare_v2(_db, sql.c_str(), -1, &stmt, nullptr)
-           != SQLITE_OK)
+        int           rc   = sqlite3_prepare_v2(_db,
+                                                sql.data(),
+                                                static_cast<int>(sql.size()),
+                                                &stmt,
+                                                nullptr);
+        if(rc != SQLITE_OK)
         {
             _last_err = sqlite3_errmsg(_db);
-            return -1;
+            return std::make_error_code(std::errc::bad_message);
         }
 
-        if(sqlite3_step(stmt) != SQLITE_ROW)
+        if(!_bind_stmt_args(stmt, args, count))
         {
             sqlite3_finalize(stmt);
-            return -1;
+            return std::make_error_code(std::errc::invalid_argument);
         }
 
-        int64_t seq = sqlite3_column_int64(stmt, 0);
+        int                    cols = sqlite3_column_count(stmt);
+        std::vector<out_val_t> temp_data;
+        std::size_t            rows_cnt = 0;
+
+        while((rc = sqlite3_step(stmt)) == SQLITE_ROW)
+        {
+            ++rows_cnt;
+            for(int col = 0; col < cols; ++col)
+            {
+                int type = sqlite3_column_type(stmt, col);
+                switch(type)
+                {
+                    case SQLITE_INTEGER:
+                        temp_data.emplace_back(static_cast<int64_t>(
+                            sqlite3_column_int64(stmt, col)));
+                        break;
+                    case SQLITE_FLOAT:
+                        temp_data.emplace_back(
+                            sqlite3_column_double(stmt, col));
+                        break;
+                    case SQLITE_TEXT: {
+                        const char *txt = reinterpret_cast<const char *>(
+                            sqlite3_column_text(stmt, col));
+                        int bytes = sqlite3_column_bytes(stmt, col);
+                        temp_data.emplace_back(std::string(txt, bytes));
+                        break;
+                    }
+                    case SQLITE_BLOB: {
+                        const uint8_t *blob_bytes =
+                            reinterpret_cast<const uint8_t *>(
+                                sqlite3_column_blob(stmt, col));
+                        int bytes = sqlite3_column_bytes(stmt, col);
+                        temp_data.emplace_back(
+                            blob_t(blob_bytes, blob_bytes + bytes));
+                        break;
+                    }
+                    case SQLITE_NULL:
+                    default:
+                        temp_data.emplace_back(nullptr);
+                        break;
+                }
+            }
+        }
+
         sqlite3_finalize(stmt);
-        return seq;
+        if(rc != SQLITE_DONE)
+        {
+            _last_err = sqlite3_errmsg(_db);
+            return std::make_error_code(std::errc::io_error);
+        }
+
+        outs.set_dim(rows_cnt, static_cast<std::size_t>(cols));
+        outs.reserve(temp_data.size());
+        for(auto &&val : temp_data)
+            outs.emplace_back(std::move(val));
+
+        return err_t();
     }
 
   private:
     sqlite(const sqlite &)            = delete;
     sqlite &operator=(const sqlite &) = delete;
-    sqlite(sqlite &&)                 = delete;
-    sqlite &operator=(sqlite &&)      = delete;
+
+    bool
+    _bind_stmt_args(sqlite3_stmt *stmt, const val_in_t *args, std::size_t count)
+    {
+        if(!args || count == 0)
+            return true;
+
+        for(std::size_t i = 0; i < count; ++i)
+        {
+            int idx = static_cast<int>(i + 1);
+            int rc  = SQLITE_OK;
+            std::visit(
+                [stmt, idx, &rc](auto &&arg) {
+                    using T = std::decay_t<decltype(arg)>;
+                    if constexpr(std::is_same_v<T, std::nullptr_t>)
+                    {
+                        rc = sqlite3_bind_null(stmt, idx);
+                    } else if constexpr(std::is_same_v<T, int64_t>)
+                    {
+                        rc = sqlite3_bind_int64(stmt, idx, arg);
+                    } else if constexpr(std::is_same_v<T, double>)
+                    {
+                        rc = sqlite3_bind_double(stmt, idx, arg);
+                    } else if constexpr(std::is_same_v<T, std::string_view>)
+                    {
+                        rc = sqlite3_bind_text(stmt,
+                                               idx,
+                                               arg.data(),
+                                               static_cast<int>(arg.size()),
+                                               SQLITE_TRANSIENT);
+                    } else if constexpr(std::is_same_v<T, blob_view>)
+                    {
+                        rc = sqlite3_bind_blob(stmt,
+                                               idx,
+                                               arg.data,
+                                               static_cast<int>(arg.size),
+                                               SQLITE_TRANSIENT);
+                    }
+                },
+                args[i]);
+
+            if(rc != SQLITE_OK)
+            {
+                _last_err = sqlite3_errmsg(_db);
+                return false;
+            }
+        }
+        return true;
+    }
 
   private:
-    sqlite3    *_db = nullptr;
+    sqlite3    *_db{nullptr};
+    std::string _db_path;
     std::string _last_err;
 };
 
 } // namespace hj
 
-#endif
+#endif // SQLITE_HPP
--- tests/sqlite_test.cpp
+++ tests/sqlite_test.cpp
@@ -1,36 +1,23 @@
 #include <gtest/gtest.h>
 #include <hj/db/sqlite.hpp>
+
 #include <cstdio>
 #include <string>
+#include <system_error>
+#include <utility>
+#include <vector>
 
 using namespace hj;
 
-bool _is_sqlite_valid()
+static bool _is_sqlite_valid()
 {
-    try
-    {
-        sqlite db;
-        if(!db.open("test_check.db"))
-            return false;
-
-        db.close();
-        remove("test_check.db");
-        return true;
-    }
-    catch(...)
-    {
+    sqlite db;
+    if(!db.open("test_check.db"))
         return false;
-    }
-}
 
-int _sqlite_exec_cb(void *in, int argc, char **argv, char **col_name)
-{
-    auto *rows = static_cast<std::vector<std::vector<std::string>> *>(in);
-    std::vector<std::string> row;
-    for(int i = 0; i < argc; ++i)
-        row.push_back(argv[i] ? argv[i] : "");
-    rows->push_back(row);
-    return 0;
+    db.close();
+    std::remove("test_check.db");
+    return true;
 }
 
 TEST(sqlite, mprintf)
@@ -45,9 +32,6 @@ TEST(sqlite, mprintf)
     str = sqlite::mprintf("Percent %% sign");
     EXPECT_EQ(str, "Percent % sign");
 
-    str = sqlite::mprintf("Hello %1, %1", "World", 123);
-    EXPECT_EQ(str, "Hello ");
-
     str = sqlite::mprintf("Hello %q, %d, %lld", "World", 123, 456789LL);
     EXPECT_EQ(str, "Hello World, 123, 456789");
 
@@ -61,107 +45,202 @@ TEST(sqlite, mprintf)
     EXPECT_EQ(str, "Empty string: ''");
 }
 
-TEST(sqlite, open_close)
+TEST(sqlite, open_close_and_path)
 {
     if(!_is_sqlite_valid())
         GTEST_SKIP() << "sqlite not available";
 
     sqlite db;
+    EXPECT_FALSE(db.is_open());
+
     EXPECT_TRUE(db.open("OpenCloseTest.db"));
     EXPECT_TRUE(db.is_open());
+    EXPECT_EQ(db.path(), "OpenCloseTest.db");
+
     db.close();
     EXPECT_FALSE(db.is_open());
-    remove("OpenCloseTest.db");
+
+    std::remove("OpenCloseTest.db");
 }
 
-TEST(sqlite, exec_and_query)
+TEST(sqlite, move_semantics)
+{
+    if(!_is_sqlite_valid())
+        GTEST_SKIP() << "sqlite not available";
+
+    sqlite db1("MoveTest.db");
+    ASSERT_TRUE(db1.is_open());
+
+    sqlite db2(std::move(db1));
+    EXPECT_FALSE(db1.is_open());
+    EXPECT_TRUE(db2.is_open());
+    EXPECT_EQ(db2.path(), "MoveTest.db");
+
+    sqlite db3;
+    db3 = std::move(db2);
+    EXPECT_FALSE(db2.is_open());
+    EXPECT_TRUE(db3.is_open());
+    EXPECT_EQ(db3.path(), "MoveTest.db");
+
+    db3.close();
+    std::remove("MoveTest.db");
+}
+
+TEST(sqlite, exec_and_query_types)
 {
     if(!_is_sqlite_valid())
         GTEST_SKIP() << "sqlite not available";
 
     sqlite db;
-    ASSERT_TRUE(db.open("ExecAndQueryTest.db"));
-    EXPECT_TRUE(db.exec("DROP TABLE IF EXISTS t;"));
-    EXPECT_TRUE(db.exec(
-        "CREATE TABLE t (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);"));
-    EXPECT_TRUE(db.exec("INSERT INTO t (name) VALUES ('Alice'),('Bob');"));
-    EXPECT_TRUE(db.sequence("t") > -1);
-    auto rows = db.query("SELECT id, name FROM t ORDER BY id;");
-    ASSERT_EQ(rows.size(), 2u);
-    EXPECT_EQ(rows[0][1], "Alice");
-    EXPECT_EQ(rows[1][1], "Bob");
+    ASSERT_TRUE(db.open("ExecQueryTypesTest.db"));
 
-    EXPECT_TRUE(db.exec("INSERT INTO t (name) VALUES ('C''arol');"));
-    EXPECT_TRUE(db.sequence("t") > -1);
-    rows = db.query("SELECT id, name FROM t WHERE name='C''arol';");
-    ASSERT_EQ(rows.size(), 1u);
-    EXPECT_EQ(rows[0][1], "C'arol");
+    auto drop_res = db.exec("DROP TABLE IF EXISTS t_all_types;");
+    EXPECT_FALSE(drop_res.ec);
 
-    EXPECT_TRUE(db.exec("INSERT INTO t (name) VALUES (''),('');"));
-    EXPECT_TRUE(db.sequence("t") > -1);
-    rows = db.query("SELECT name FROM t WHERE name='';");
-    ASSERT_EQ(rows.size(), 2u);
+    auto create_res = db.exec("CREATE TABLE t_all_types ("
+                              "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                              "i_val INTEGER, "
+                              "f_val REAL, "
+                              "s_val TEXT, "
+                              "b_val BLOB, "
+                              "n_val TEXT);");
+    EXPECT_FALSE(create_res.ec);
 
-    EXPECT_TRUE(db.exec("UPDATE t SET name='Dave' WHERE name='Bob';"));
-    rows = db.query("SELECT name FROM t WHERE id=2;");
-    ASSERT_EQ(rows.size(), 1u);
-    EXPECT_EQ(rows[0][0], "Dave");
+    int64_t            i_in       = 123456789012345LL;
+    double             f_in       = 2.718281828;
+    std::string_view   s_in       = "sqlite_test_string";
+    uint8_t            raw_blob[] = {0xAA, 0xBB, 0xCC, 0x00, 0xDD, 0xEE};
+    db_conn::blob_view b_in{raw_blob, sizeof(raw_blob)};
+
+    auto ins_res =
+        db.exec("INSERT INTO t_all_types (i_val, f_val, s_val, b_val, n_val) "
+                "VALUES (?, ?, ?, ?, ?);",
+                i_in,
+                f_in,
+                s_in,
+                b_in,
+                nullptr);
+
+    EXPECT_FALSE(ins_res.ec);
+    EXPECT_EQ(ins_res.affected_rows, 1);
+    EXPECT_GT(ins_res.last_insert_id, 0);
+
+    db_conn::ret_t outs;
+    auto query_err = db.query(outs,
+                              "SELECT i_val, f_val, s_val, b_val, n_val "
+                              "FROM t_all_types WHERE id = ?;",
+                              ins_res.last_insert_id);
+
+    EXPECT_FALSE(query_err);
+    ASSERT_EQ(outs.rows(), 1u);
+    ASSERT_EQ(outs.cols(), 5u);
+
+    EXPECT_EQ(outs.get_or<int64_t>(0, 0, 0), i_in);
+    EXPECT_NEAR(outs.get_or<double>(0, 1, 0.0), f_in, 1e-6);
+    EXPECT_EQ(outs.get_or<std::string>(0, 2, ""), "sqlite_test_string");
+
+    const auto *p_blob = outs.get_if<db_conn::blob_t>(0, 3);
+    ASSERT_NE(p_blob, nullptr);
+    db_conn::blob_t expected_blob(raw_blob, raw_blob + sizeof(raw_blob));
+    EXPECT_EQ(*p_blob, expected_blob);
+
+    EXPECT_TRUE(outs.is_null(0, 4));
 
     db.close();
-    remove("ExecAndQueryTest.db");
+    std::remove("ExecQueryTypesTest.db");
 }
-TEST(sqlite, transaction)
+
+TEST(sqlite, transaction_manual)
 {
     if(!_is_sqlite_valid())
         GTEST_SKIP() << "sqlite not available";
 
     sqlite db;
-    ASSERT_TRUE(db.open("TransTest.db"));
-    EXPECT_TRUE(db.exec("DROP TABLE IF EXISTS t;"));
-    EXPECT_TRUE(db.exec("CREATE TABLE t (id INTEGER PRIMARY KEY, v TEXT);"));
-    EXPECT_TRUE(db.begin());
-    EXPECT_TRUE(db.exec("INSERT INTO t (v) VALUES ('x');"));
-    EXPECT_TRUE(db.rollback());
-    auto rows = db.query("SELECT * FROM t;");
-    EXPECT_TRUE(rows.empty());
-    EXPECT_TRUE(db.begin());
-    EXPECT_TRUE(db.exec("INSERT INTO t (v) VALUES ('y');"));
-    EXPECT_TRUE(db.commit());
-    rows = db.query("SELECT v FROM t;");
-    ASSERT_EQ(rows.size(), 1u);
-    EXPECT_EQ(rows[0][0], "y");
+    ASSERT_TRUE(db.open("TransManualTest.db"));
+
+    db.exec("DROP TABLE IF EXISTS t_trans;");
+    db.exec("CREATE TABLE t_trans (id INTEGER PRIMARY KEY, v TEXT);");
+
+    EXPECT_FALSE(db.begin());
+    db.exec("INSERT INTO t_trans (v) VALUES ('rolled_back');");
+    EXPECT_FALSE(db.rollback());
+
+    db_conn::ret_t outs;
+    db.query(outs, "SELECT * FROM t_trans;");
+    EXPECT_TRUE(outs.empty());
+
+    EXPECT_FALSE(db.begin());
+    db.exec("INSERT INTO t_trans (v) VALUES ('committed');");
+    EXPECT_FALSE(db.commit());
+
+    db.query(outs, "SELECT v FROM t_trans;");
+    ASSERT_EQ(outs.rows(), 1u);
+    EXPECT_EQ(outs.get_or<std::string>(0, 0, ""), "committed");
+
     db.close();
-    remove("TransTest.db");
+    std::remove("TransManualTest.db");
 }
 
-TEST(sqlite, exec_error)
+TEST(sqlite, transaction_guard)
 {
     if(!_is_sqlite_valid())
         GTEST_SKIP() << "sqlite not available";
+
+    sqlite db;
+    ASSERT_TRUE(db.open("TransGuardTest.db"));
+
+    db.exec("DROP TABLE IF EXISTS t_guard;");
+    db.exec("CREATE TABLE t_guard (id INTEGER PRIMARY KEY, v TEXT);");
+
+    {
+        db_conn::trans_guard guard(db);
+        db.exec("INSERT INTO t_guard (v) VALUES ('rollback_value');");
+    }
+
+    db_conn::ret_t outs;
+    db.query(outs, "SELECT * FROM t_guard;");
+    EXPECT_TRUE(outs.empty());
+
+    {
+        db_conn::trans_guard guard(db);
+        db.exec("INSERT INTO t_guard (v) VALUES ('commit_value');");
+        EXPECT_FALSE(guard.commit());
+    }
+
+    db.query(outs, "SELECT v FROM t_guard;");
+    ASSERT_EQ(outs.rows(), 1u);
+    EXPECT_EQ(outs.get_or<std::string>(0, 0, ""), "commit_value");
+
+    db.close();
+    std::remove("TransGuardTest.db");
+}
+
+TEST(sqlite, error_handling)
+{
+    if(!_is_sqlite_valid())
+        GTEST_SKIP() << "sqlite not available";
+
     sqlite db;
     ASSERT_TRUE(db.open("ExecErrTest.db"));
-    EXPECT_FALSE(db.exec("INVALID SQL;"));
-    std::string err = db.get_last_error();
-    EXPECT_FALSE(err.empty());
-    db.close();
-    remove("ExecErrTest.db");
-}
 
-TEST(sqlite, query_no_table)
-{
-    if(!_is_sqlite_valid())
-        GTEST_SKIP() << "sqlite not available";
-    sqlite db;
-    ASSERT_TRUE(db.open("NoTableTest.db"));
-    auto rows = db.query("SELECT * FROM not_exist;");
-    EXPECT_TRUE(rows.empty());
+    auto res = db.exec("INVALID SQL STATEMENT;");
+    EXPECT_TRUE(res.ec);
+    std::string err_msg = db.get_last_error();
+    EXPECT_FALSE(err_msg.empty());
+
+    db_conn::ret_t outs;
+    auto           q_err = db.query(outs, "SELECT * FROM non_existent_table;");
+    EXPECT_TRUE(q_err);
+    EXPECT_TRUE(outs.empty());
+    EXPECT_FALSE(db.get_last_error().empty());
+
     db.close();
-    remove("NoTableTest.db");
+    std::remove("ExecErrTest.db");
 }
 
 TEST(sqlite, open_close_many)
 {
-    for(int i = 0; i < 10; ++i)
+    for(int i = 0; i < 5; ++i)
     {
         sqlite db;
         EXPECT_TRUE(db.open("OpenCloseManyTest.db"));
@@ -169,35 +248,5 @@ TEST(sqlite, open_close_many)
         db.close();
         EXPECT_FALSE(db.is_open());
     }
-    remove("OpenCloseManyTest.db");
-}
-
-TEST(sqlite, query_empty)
-{
-    if(!_is_sqlite_valid())
-        GTEST_SKIP() << "sqlite not available";
-
-    sqlite db;
-    ASSERT_TRUE(db.open("QueryEmptyTest.db"));
-    EXPECT_TRUE(db.exec("DROP TABLE IF EXISTS t;"));
-    db.exec("CREATE TABLE t (id INTEGER);");
-    auto rows = db.query("SELECT * FROM t;");
-    EXPECT_TRUE(rows.empty());
-    db.close();
-    remove("QueryEmptyTest.db");
-}
-
-TEST(sqlite, get_last_error)
-{
-    if(!_is_sqlite_valid())
-        GTEST_SKIP() << "sqlite not available";
-
-    sqlite db;
-    ASSERT_TRUE(db.open("GetLastErrorTest.db"));
-    EXPECT_FALSE(db.exec("SELECT * FROM not_exist_table;"));
-    std::string err = db.get_last_error();
-    EXPECT_FALSE(err.empty());
-    EXPECT_NE(err.find("no such table"), std::string::npos);
-    db.close();
-    remove("GetLastErrorTest.db");
+    std::remove("OpenCloseManyTest.db");
 }

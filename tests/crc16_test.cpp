aea56207d0ab57062e907111f134273dbdc388f0
hanjingo <hehehunanchina@live.com>
2026-08-15 01:17:36 +0800

    upgrade crc16/crc32

--- tests/crc16_test.cpp
+++ tests/crc16_test.cpp
@@ -1,148 +1,149 @@
 #include <gtest/gtest.h>
-
-// #define CRC16_MODBUS_ENABLED
-#define CRC16_XMODEM_ENABLED
-#include <hj/algo/crc16.h>
 #include <cstring>
+#include <vector>
 
-TEST(crc16, standard_test_vectors)
+#ifndef HJ_CRC16_MODBUS_ENABLED
+#define HJ_CRC16_XMODEM_ENABLED
+#endif
+
+#include <hj/algo/crc16.h>
+
+TEST(hj_crc16, standard_test_vectors)
 {
-    const char *test1   = "123456789";
-    uint16_t    result1 = crc16(test1, strlen(test1), 0);
-#if CRC16_ALGO == CRC16_MODBUS
-    EXPECT_EQ(result1, 0x4B37)
-        << "Standard test vector '123456789' failed, actual: 0x" << std::hex
-        << result1;
-#elif CRC16_ALGO == CRC16_XMODEM
-    EXPECT_EQ(result1, 0x31C3)
-        << "Standard test vector '123456789' failed, actual: 0x" << std::hex
-        << result1;
+    const char *test1 = "123456789";
+#if HJ_CRC16_ALGO == HJ_CRC16_MODBUS
+    EXPECT_EQ(hj_crc16(test1, strlen(test1)), 0x4B37);
+    EXPECT_EQ(hj_crc16_modbus(test1, strlen(test1)), 0x4B37);
+#elif HJ_CRC16_ALGO == HJ_CRC16_XMODEM
+    EXPECT_EQ(hj_crc16(test1, strlen(test1)), 0x31C3);
+    EXPECT_EQ(hj_crc16_xmodem(test1, strlen(test1)), 0x31C3);
 #endif
 
-    const char *test2   = "";
-    uint16_t    result2 = crc16(test2, strlen(test2), 0);
-#if CRC16_ALGO == CRC16_MODBUS
-    EXPECT_EQ(result2, 0xFFFF)
-        << "Empty string test failed, actual: 0x" << std::hex << result2;
-#elif CRC16_ALGO == CRC16_XMODEM
-    EXPECT_EQ(result2, 0x0000)
-        << "Empty string test failed, actual: 0x" << std::hex << result2;
+    const char *test2 = "";
+#if HJ_CRC16_ALGO == HJ_CRC16_MODBUS
+    EXPECT_EQ(hj_crc16(test2, strlen(test2)), 0xFFFFU);
+#elif HJ_CRC16_ALGO == HJ_CRC16_XMODEM
+    EXPECT_EQ(hj_crc16(test2, strlen(test2)), 0x0000U);
 #endif
 
-    const char *test3   = "a";
-    uint16_t    result3 = crc16(test3, strlen(test3), 0);
-#if CRC16_ALGO == CRC16_MODBUS
-    EXPECT_NE(result3, 0) << "Single character 'a' should produce non-zero CRC";
-#elif CRC16_ALGO == CRC16_XMODEM
-    EXPECT_NE(result3, 0) << "Single character 'a' should produce non-zero CRC";
+    const char *test3 = "a";
+    EXPECT_NE(hj_crc16(test3, strlen(test3)), 0U);
+
+    const char *test4 = "abc";
+    EXPECT_NE(hj_crc16(test4, strlen(test4)), 0U);
+}
+
+TEST(hj_crc16, context_lifecycle_and_reset)
+{
+    hj_crc16_ctx_t ctx;
+
+    hj_crc16_init(&ctx);
+#if HJ_CRC16_ALGO == HJ_CRC16_MODBUS
+    EXPECT_EQ(ctx.state, 0xFFFFU);
+#elif HJ_CRC16_ALGO == HJ_CRC16_XMODEM
+    EXPECT_EQ(ctx.state, 0x0000U);
 #endif
 
-    const char *test4   = "abc";
-    uint16_t    result4 = crc16(test4, strlen(test4), 0);
-#if CRC16_ALGO == CRC16_MODBUS
-    EXPECT_NE(result4, 0) << "String 'abc' should produce non-zero CRC";
-#elif CRC16_ALGO == CRC16_XMODEM
-    EXPECT_NE(result4, 0) << "String 'abc' should produce non-zero CRC";
+    const char *data = "The quick brown fox jumps over the lazy dog";
+    hj_crc16_update(&ctx, data, strlen(data));
+    uint16_t crc1 = hj_crc16_finalize(&ctx);
+
+    EXPECT_EQ(crc1, hj_crc16(data, strlen(data)));
+
+    hj_crc16_init(&ctx);
+    hj_crc16_update(&ctx, data, strlen(data));
+    uint16_t crc2 = hj_crc16_finalize(&ctx);
+    EXPECT_EQ(crc1, crc2);
+}
+
+TEST(hj_crc16, incremental_and_chunked)
+{
+    const char *full_data = "The quick brown fox jumps over the lazy dog";
+    size_t      len       = strlen(full_data);
+    uint16_t    expected  = hj_crc16(full_data, len);
+
+    hj_crc16_ctx_t ctx_byte;
+    hj_crc16_init(&ctx_byte);
+    for(size_t i = 0; i < len; ++i)
+    {
+        hj_crc16_update(&ctx_byte, &full_data[i], 1);
+    }
+    EXPECT_EQ(hj_crc16_finalize(&ctx_byte), expected);
+
+    hj_crc16_ctx_t ctx_chunk;
+    hj_crc16_init(&ctx_chunk);
+    size_t chunks[] = {3, 5, 7, 2, 10};
+    size_t offset   = 0;
+
+    for(size_t chunk_len : chunks)
+    {
+        if(offset + chunk_len <= len)
+        {
+            hj_crc16_update(&ctx_chunk, full_data + offset, chunk_len);
+            offset += chunk_len;
+        }
+    }
+    if(offset < len)
+    {
+        hj_crc16_update(&ctx_chunk, full_data + offset, len - offset);
+    }
+    EXPECT_EQ(hj_crc16_finalize(&ctx_chunk), expected);
+}
+
+TEST(hj_crc16, null_and_boundary_conditions)
+{
+    const char *data = "test_boundary_data";
+
+    hj_crc16_init(nullptr);
+    hj_crc16_update(nullptr, data, strlen(data));
+#if HJ_CRC16_ALGO == HJ_CRC16_MODBUS
+    EXPECT_EQ(hj_crc16_finalize(nullptr), 0xFFFFU);
+#elif HJ_CRC16_ALGO == HJ_CRC16_XMODEM
+    EXPECT_EQ(hj_crc16_finalize(nullptr), 0x0000U);
+#endif
+
+    hj_crc16_ctx_t ctx;
+    hj_crc16_init(&ctx);
+    uint16_t initial_state = ctx.state;
+
+    hj_crc16_update(&ctx, nullptr, 10);
+    EXPECT_EQ(ctx.state, initial_state);
+
+    hj_crc16_update(&ctx, data, 0);
+    EXPECT_EQ(ctx.state, initial_state);
+
+#if HJ_CRC16_ALGO == HJ_CRC16_MODBUS
+    EXPECT_EQ(hj_crc16(nullptr, 0), 0xFFFFU);
+    EXPECT_EQ(hj_crc16(nullptr, 10), 0xFFFFU);
+    EXPECT_EQ(hj_crc16(data, 0), 0xFFFFU);
+#elif HJ_CRC16_ALGO == HJ_CRC16_XMODEM
+    EXPECT_EQ(hj_crc16(nullptr, 0), 0x0000U);
+    EXPECT_EQ(hj_crc16(nullptr, 10), 0x0000U);
+    EXPECT_EQ(hj_crc16(data, 0), 0x0000U);
 #endif
 }
 
-TEST(crc16, large_text_crc)
+TEST(hj_crc16, binary_data_coverage)
+{
+    std::vector<uint8_t> all_bytes(256);
+    for(int i = 0; i < 256; ++i)
+    {
+        all_bytes[i] = static_cast<uint8_t>(i);
+    }
+
+    uint16_t result = hj_crc16(all_bytes.data(), all_bytes.size());
+    EXPECT_NE(result, 0U);
+
+    uint8_t zero_byte = 0x00;
+    uint8_t ff_byte   = 0xFF;
+    EXPECT_NE(hj_crc16(&zero_byte, 1), hj_crc16(&ff_byte, 1));
+}
+
+TEST(hj_crc16, large_text_crc)
 {
     const char *big_data =
         R"(The class everyone had really been looking forward to was Defence Against the Dark Arts, but Quirrell's lessons turned out to be a bit of a joke.His classroom smelled strongly of garlic, which everyone said was to ward off a vampire he'd met in Romania, and was afraid would be coming back to get him one of these days.His turban, he told them, had been given to him by an African prince as a thank-you for getting rid of a troublesome zombie, but they weren't sure they believed this story.For one thing, when Seamus Finnegan asked eagerly to hear how Quirrell had fought off the zombie, Quirrell went pink and sta)";
 
-    uint16_t result = crc16(big_data, strlen(big_data), 0);
-
-    EXPECT_NE(result, 0) << "Large text should produce non-zero CRC";
-    EXPECT_NE(result, 0xFFFF) << "Large text should not produce initial value";
-}
-
-TEST(crc16, incremental_calculation)
-{
-    const char *part1 = "Hello, ";
-    const char *part2 = "World!";
-
-    std::string combined = std::string(part1) + part2;
-    uint16_t    full_crc = crc16(combined.c_str(), combined.length(), 0);
-
-    uint16_t crc1 = crc16(part1, strlen(part1), 0);
-    uint16_t crc2 = crc16(part2, strlen(part2), crc1);
-
-#if CRC16_ALGO == CRC16_MODBUS
-    EXPECT_EQ(full_crc, crc2)
-        << "Incremental CRC calculation should match full calculation";
-#elif CRC16_ALGO == CRC16_XMODEM
-    EXPECT_EQ(full_crc, crc2)
-        << "Incremental CRC calculation should match full calculation";
-#endif
-}
-
-TEST(crc16, edge_cases)
-{
-    const char *data = "test";
-
-#if CRC16_ALGO == CRC16_MODBUS
-    EXPECT_EQ(crc16(nullptr, 0, 0), 0xFFFF)
-        << "CRC of null data with zero length should be initial value (0xFFFF)";
-    EXPECT_EQ(crc16(nullptr, 10, 123), 123);
-    EXPECT_EQ(crc16(data, 0, 0), 0xFFFF)
-        << "CRC of zero length data should be initial value (0xFFFF)";
-#elif CRC16_ALGO == CRC16_XMODEM
-    EXPECT_EQ(crc16(nullptr, 0, 0), 0x0000)
-        << "CRC of null data with zero length should be initial value (0x0000)";
-    EXPECT_EQ(crc16(nullptr, 10, 123), 123);
-    EXPECT_EQ(crc16(data, 0, 0), 0x0000)
-        << "CRC of zero length data should be initial value (0x0000)";
-#endif
-}
-
-TEST(crc16, consistency_check)
-{
-    const char *test_data = "The quick brown fox jumps over the lazy dog";
-
-    uint16_t crc1 = crc16(test_data, strlen(test_data), 0);
-    uint16_t crc2 = crc16(test_data, strlen(test_data), 0);
-    uint16_t crc3 = crc16(test_data, strlen(test_data), 0);
-
-    EXPECT_EQ(crc1, crc2);
-    EXPECT_EQ(crc2, crc3);
-
-    const char *different_data = "The quick brown fox jumps over the lazy cat";
-    uint16_t different_crc = crc16(different_data, strlen(different_data), 0);
-
-#if CRC16_ALGO == CRC16_MODBUS
-    EXPECT_NE(crc1, different_crc)
-        << "Different data should produce different CRC";
-#elif CRC16_ALGO == CRC16_XMODEM
-    EXPECT_NE(crc1, different_crc)
-        << "Different data should produce different CRC";
-#endif
-}
-
-TEST(crc16, incremental_detailed)
-{
-    const char *parts[]   = {"The ", "quick ", "brown ", "fox"};
-    size_t      num_parts = sizeof(parts) / sizeof(parts[0]);
-
-    std::string full_string;
-    for(size_t i = 0; i < num_parts; ++i)
-    {
-        full_string += parts[i];
-    }
-
-    uint16_t full_crc = crc16(full_string.c_str(), full_string.length(), 0);
-
-    uint16_t incremental_crc = 0;
-    for(size_t i = 0; i < num_parts; ++i)
-    {
-        incremental_crc = crc16(parts[i], strlen(parts[i]), incremental_crc);
-    }
-
-#if CRC16_ALGO == CRC16_MODBUS
-    EXPECT_EQ(full_crc, incremental_crc)
-        << "Multi-part incremental calculation should match full calculation";
-#elif CRC16_ALGO == CRC16_XMODEM
-    EXPECT_EQ(full_crc, incremental_crc)
-        << "Multi-part incremental calculation should match full calculation";
-#endif
+    uint16_t result = hj_crc16(big_data, strlen(big_data));
+    EXPECT_NE(result, 0U);
 }
--- tests/crc32_test.cpp
+++ tests/crc32_test.cpp
@@ -1,52 +1,115 @@
 #include <gtest/gtest.h>
+#include <cstring>
+#include <vector>
+#include <string>
+
 #include <hj/algo/crc32.h>
 
-TEST(crc32, standard_test_vectors)
+TEST(hj_crc32, standard_test_vectors)
 {
-    const char *test1   = "123456789";
-    uint32_t    result1 = crc32(test1, strlen(test1), 0);
-    EXPECT_EQ(result1, 0xCBF43926) << "Standard test vector '123456789' failed";
+    const char *test1 = "123456789";
+    EXPECT_EQ(hj_crc32(test1, strlen(test1)), 0xCBF43926U);
+    EXPECT_EQ(hj_crc32_iso_hdlc(test1, strlen(test1)), 0xCBF43926U);
 
-    const char *test2   = "";
-    uint32_t    result2 = crc32(test2, strlen(test2), 0);
-    EXPECT_EQ(result2, 0x00000000) << "Empty string test failed";
+    const char *test2 = "";
+    EXPECT_EQ(hj_crc32(test2, strlen(test2)), 0x00000000U);
 
-    const char *test3   = "a";
-    uint32_t    result3 = crc32(test3, strlen(test3), 0);
-    EXPECT_EQ(result3, 0xE8B7BE43) << "Single character 'a' test failed";
+    const char *test3 = "a";
+    EXPECT_EQ(hj_crc32(test3, strlen(test3)), 0xE8B7BE43U);
 
-    const char *test4   = "abc";
-    uint32_t    result4 = crc32(test4, strlen(test4), 0);
-    EXPECT_EQ(result4, 0x352441C2) << "String 'abc' test failed";
+    const char *test4 = "abc";
+    EXPECT_EQ(hj_crc32(test4, strlen(test4)), 0x352441C2U);
+
+    const char *test5 = "message digest";
+    EXPECT_EQ(hj_crc32(test5, strlen(test5)), 0x20159D7FU);
 }
 
-TEST(crc32, large_text_crc)
+TEST(hj_crc32, context_lifecycle_and_reset)
+{
+    hj_crc32_ctx_t ctx;
+
+    hj_crc32_init(&ctx);
+    EXPECT_EQ(ctx.state, 0xFFFFFFFFU);
+
+    const char *data = "The quick brown fox jumps over the lazy dog";
+    hj_crc32_update(&ctx, data, strlen(data));
+    uint32_t crc1 = hj_crc32_finalize(&ctx);
+
+    EXPECT_EQ(crc1, hj_crc32(data, strlen(data)));
+
+    hj_crc32_init(&ctx);
+    hj_crc32_update(&ctx, data, strlen(data));
+    uint32_t crc2 = hj_crc32_finalize(&ctx);
+    EXPECT_EQ(crc1, crc2);
+}
+
+TEST(hj_crc32, incremental_and_chunked)
+{
+    const char *part1    = "Hello, ";
+    const char *part2    = "World!";
+    std::string combined = std::string(part1) + part2;
+
+    uint32_t expected = hj_crc32(combined.c_str(), combined.length());
+
+    hj_crc32_ctx_t ctx;
+    hj_crc32_init(&ctx);
+    hj_crc32_update(&ctx, part1, strlen(part1));
+    hj_crc32_update(&ctx, part2, strlen(part2));
+    uint32_t stream_crc = hj_crc32_finalize(&ctx);
+    EXPECT_EQ(stream_crc, expected);
+
+    hj_crc32_ctx_t ctx_byte;
+    hj_crc32_init(&ctx_byte);
+    for(size_t i = 0; i < combined.length(); ++i)
+    {
+        hj_crc32_update(&ctx_byte, &combined[i], 1);
+    }
+    EXPECT_EQ(hj_crc32_finalize(&ctx_byte), expected);
+}
+
+TEST(hj_crc32, null_and_boundary_conditions)
+{
+    const char *data = "test_data";
+
+    hj_crc32_init(nullptr);
+    hj_crc32_update(nullptr, data, strlen(data));
+    EXPECT_EQ(hj_crc32_finalize(nullptr), 0U);
+
+    hj_crc32_ctx_t ctx;
+    hj_crc32_init(&ctx);
+    uint32_t initial_state = ctx.state;
+
+    hj_crc32_update(&ctx, nullptr, 10);
+    EXPECT_EQ(ctx.state, initial_state);
+
+    hj_crc32_update(&ctx, data, 0);
+    EXPECT_EQ(ctx.state, initial_state);
+
+    EXPECT_EQ(hj_crc32(nullptr, 0), 0U);
+    EXPECT_EQ(hj_crc32(nullptr, 10), 0U);
+    EXPECT_EQ(hj_crc32(data, 0), 0U);
+}
+
+TEST(hj_crc32, binary_data_coverage)
+{
+    std::vector<uint8_t> all_bytes(256);
+    for(int i = 0; i < 256; ++i)
+    {
+        all_bytes[i] = static_cast<uint8_t>(i);
+    }
+
+    uint32_t full_crc = hj_crc32(all_bytes.data(), all_bytes.size());
+    EXPECT_EQ(full_crc, 0x29058C73U);
+
+    uint8_t zero_byte = 0x00;
+    uint8_t ff_byte   = 0xFF;
+    EXPECT_NE(hj_crc32(&zero_byte, 1), hj_crc32(&ff_byte, 1));
+}
+
+TEST(hj_crc32, large_text_crc)
 {
     const char *big_data =
         R"(The class everyone had really been looking forward to was Defence Against the Dark Arts, but Quirrell's lessons turned out to be a bit of a joke.His classroom smelled strongly of garlic, which everyone said was to ward off a vampire he'd met in Romania, and was afraid would be coming back to get him one of these days.His turban, he told them, had been given to him by an African prince as a thank-you for getting rid of a troublesome zombie, but they weren't sure they believed this story.For one thing, when Seamus Finnegan asked eagerly to hear how Quirrell had fought off the zombie, Quirrell went pink and sta)";
-    EXPECT_EQ(crc32(big_data, strlen(big_data), 0), 0x2E865E47);
-}
 
-TEST(crc32, incremental_calculation)
-{
-    const char *part1 = "Hello, ";
-    const char *part2 = "World!";
-
-    std::string combined = std::string(part1) + part2;
-    uint32_t    full_crc = crc32(combined.c_str(), combined.length(), 0);
-
-    uint32_t crc1 = crc32(part1, strlen(part1), 0);
-    uint32_t crc2 = crc32(part2, strlen(part2), crc1);
-
-    EXPECT_EQ(full_crc, crc2)
-        << "Incremental CRC calculation should match full calculation";
-}
-
-TEST(crc32, edge_cases)
-{
-    // EXPECT_EQ(crc32(nullptr, 0, 0), 0);
-    // EXPECT_EQ(crc32(nullptr, 10, 123), 123);
-
-    const char *data = "test";
-    EXPECT_EQ(crc32(data, 0, 0), 0);
+    EXPECT_EQ(hj_crc32(big_data, strlen(big_data)), 0x2E865E47U);
 }

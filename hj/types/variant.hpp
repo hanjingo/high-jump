88cbe81ce354848d5e03f2e2f6df77754d7c406b
hanjingo <hehehunanchina@live.com>
2026-08-22 09:46:12 +0800

    upgrade variant

--- hj/types/variant.hpp
+++ hj/types/variant.hpp
@@ -1,29 +1,23 @@
-/*
- *  This file is part of high-jump(hj).
- *  Copyright (C) 2025 hanjingo <hehehunanchina@live.com>
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef VARIANT_HPP
 #define VARIANT_HPP
 
-#if __has_include(<variant>)
+#include <ciso646>
+
+#define HJ_HAS_VARIANT 0
+
+#if defined(__has_include)
+#if __has_include(<variant>) && ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || __cplusplus >= 201703L)
+#undef HJ_HAS_VARIANT
+#define HJ_HAS_VARIANT 1
+#endif
+#endif
+
+#if HJ_HAS_VARIANT
 #include <variant>
 
 namespace hj
 {
+
 template <typename... T>
 using variant = std::variant<T...>;
 
@@ -31,66 +25,235 @@ template <class T, class... Types>
 constexpr T &get(std::variant<Types...> &v)
 {
     return std::get<T>(v);
-};
+}
 
 template <class T, class... Types>
 constexpr T &&get(std::variant<Types...> &&v)
 {
     return std::get<T>(std::move(v));
-};
+}
 
 template <class T, class... Types>
 constexpr const T &get(const std::variant<Types...> &v)
 {
     return std::get<T>(v);
-};
+}
 
 template <class T, class... Types>
 constexpr const T &&get(const std::variant<Types...> &&v)
 {
     return std::get<T>(std::move(v));
-};
-
 }
 
-#elif __has_include(<boost/variant.hpp>)
+template <size_t I, class... Types>
+constexpr auto &get(std::variant<Types...> &v)
+{
+    return std::get<I>(v);
+}
+
+template <size_t I, class... Types>
+constexpr auto &&get(std::variant<Types...> &&v)
+{
+    return std::get<I>(std::move(v));
+}
+
+template <size_t I, class... Types>
+constexpr const auto &get(const std::variant<Types...> &v)
+{
+    return std::get<I>(v);
+}
+
+template <size_t I, class... Types>
+constexpr const auto &&get(const std::variant<Types...> &&v)
+{
+    return std::get<I>(std::move(v));
+}
+
+template <class T, class... Types>
+constexpr T *get_if(std::variant<Types...> *v) noexcept
+{
+    return std::get_if<T>(v);
+}
+
+template <class T, class... Types>
+constexpr const T *get_if(const std::variant<Types...> *v) noexcept
+{
+    return std::get_if<T>(v);
+}
+
+template <size_t I, class... Types>
+constexpr auto *get_if(std::variant<Types...> *v) noexcept
+{
+    return std::get_if<I>(v);
+}
+
+template <size_t I, class... Types>
+constexpr const auto *get_if(const std::variant<Types...> *v) noexcept
+{
+    return std::get_if<I>(v);
+}
+
+template <class T, class... Types>
+constexpr bool holds_alternative(const std::variant<Types...> &v) noexcept
+{
+    return std::holds_alternative<T>(v);
+}
+
+template <class Visitor, class... Variants>
+constexpr decltype(auto) visit(Visitor &&vis, Variants &&...vars)
+{
+    return std::visit(std::forward<Visitor>(vis),
+                      std::forward<Variants>(vars)...);
+}
+
+template <typename T>
+struct variant_size : std::variant_size<T>
+{
+};
+
+template <size_t I, typename T>
+struct variant_alternative : std::variant_alternative<I, T>
+{
+};
+
+template <typename T>
+inline constexpr size_t variant_size_v = std::variant_size_v<T>;
+
+template <size_t I, typename T>
+using variant_alternative_t = std::variant_alternative_t<I, T>;
+
+} // namespace hj
+
+#else
+
+#define HJ_HAS_BOOST_VARIANT 0
+#if defined(__has_include)
+#if __has_include(<boost/variant.hpp>)
+#undef HJ_HAS_BOOST_VARIANT
+#define HJ_HAS_BOOST_VARIANT 1
+#endif
+#elif defined(BOOST_HAS_VARIANT_HPP)
+#define HJ_HAS_BOOST_VARIANT 1
+#endif
+
+#if HJ_HAS_BOOST_VARIANT
 #include <boost/variant.hpp>
+#include <boost/move/utility.hpp>
 
 namespace hj
 {
+
 template <typename... T>
 using variant = boost::variant<T...>;
 
 template <class T, class... Types>
-constexpr T &get(boost::variant<Types...> &v)
+T &get(boost::variant<Types...> &v)
 {
     return boost::get<T>(v);
-};
+}
 
 template <class T, class... Types>
-constexpr T &&get(boost::variant<Types...> &&v)
+T &&get(boost::variant<Types...> &&v)
 {
     return boost::get<T>(boost::move(v));
-};
+}
 
 template <class T, class... Types>
-constexpr const T &get(const boost::variant<Types...> &v)
+const T &get(const boost::variant<Types...> &v)
 {
     return boost::get<T>(v);
-};
+}
 
 template <class T, class... Types>
-constexpr const T &&get(const boost::variant<Types...> &&v)
+const T &&get(const boost::variant<Types...> &&v)
 {
-    return boost::get<T>(std::move(v));
+    return boost::get<T>(boost::move(v));
+}
+
+template <size_t I, class... Types>
+auto &get(boost::variant<Types...> &v)
+{
+    return *boost::get<I>(&v);
+}
+
+template <size_t I, class... Types>
+auto &&get(boost::variant<Types...> &&v)
+{
+    return std::move(*boost::get<I>(&v));
+}
+
+template <size_t I, class... Types>
+const auto &get(const boost::variant<Types...> &v)
+{
+    return *boost::get<I>(&v);
+}
+
+template <size_t I, class... Types>
+const auto &&get(const boost::variant<Types...> &&v)
+{
+    return std::move(*boost::get<I>(&v));
+}
+
+template <class T, class... Types>
+T *get_if(boost::variant<Types...> *v) noexcept
+{
+    return boost::get<T>(v);
+}
+
+template <class T, class... Types>
+const T *get_if(const boost::variant<Types...> *v) noexcept
+{
+    return boost::get<T>(v);
+}
+
+template <size_t I, class... Types>
+auto *get_if(boost::variant<Types...> *v) noexcept
+{
+    return boost::get<I>(v);
+}
+
+template <size_t I, class... Types>
+const auto *get_if(const boost::variant<Types...> *v) noexcept
+{
+    return boost::get<I>(v);
+}
+
+template <class T, class... Types>
+bool holds_alternative(const boost::variant<Types...> &v) noexcept
+{
+    return boost::get<T>(&v) != nullptr;
+}
+
+template <class Visitor, class... Variants>
+decltype(auto) visit(Visitor &&vis, Variants &&...vars)
+{
+    return boost::apply_visitor(std::forward<Visitor>(vis),
+                                std::forward<Variants>(vars)...);
+}
+
+template <typename T>
+struct variant_size : boost::variant_size<T>
+{
 };
 
-}
+template <size_t I, typename T>
+struct variant_alternative : boost::variant_alternative<I, T>
+{
+};
+
+template <typename T>
+inline constexpr size_t variant_size_v = boost::variant_size<T>::value;
+
+template <size_t I, typename T>
+using variant_alternative_t = typename boost::variant_alternative<I, T>::type;
+
+} // namespace hj
 
 #else
 #error                                                                         \
     "No suitable variant implementation found (need C++17 std::variant or boost::variant)"
-
 #endif
 
 #endif
+
+#endif // VARIANT_HPP
--- tests/variant_test.cpp
+++ tests/variant_test.cpp
@@ -1,7 +1,7 @@
-
 #include <gtest/gtest.h>
 #include <hj/types/variant.hpp>
 #include <string>
+#include <type_traits>
 
 TEST(variant, basic_construct_get)
 {
@@ -34,6 +34,43 @@ TEST(variant, type_switch)
     ASSERT_DOUBLE_EQ(hj::get<double>(v), 2.5);
 }
 
+TEST(variant, copy_semantics)
+{
+    hj::variant<int, std::string> a = std::string("hello");
+    auto                          b = a;
+    ASSERT_EQ(hj::get<std::string>(b), "hello");
+    ASSERT_EQ(hj::get<std::string>(a), "hello");
+}
+
+TEST(variant, move_semantics)
+{
+    hj::variant<int, std::string> a = std::string("world");
+    auto                          b = std::move(a);
+    ASSERT_EQ(hj::get<std::string>(b), "world");
+}
+
+TEST(variant, assignment)
+{
+    hj::variant<int, std::string> v = 100;
+    v                               = int{42};
+    ASSERT_EQ(hj::get<int>(v), 42);
+
+    v = std::string("assigned");
+    ASSERT_EQ(hj::get<std::string>(v), "assigned");
+}
+
+TEST(variant, self_assignment)
+{
+    hj::variant<int, std::string> v = std::string("self");
+    v                               = static_cast<decltype(v) &>(v);
+    ASSERT_EQ(hj::get<std::string>(v), "self");
+}
+
+TEST(variant, same_type_duplicates_compile_check)
+{
+    SUCCEED();
+}
+
 #if (__cplusplus >= 201703L) || (defined(_MSC_VER) && _MSC_VER >= 1910)
 TEST(variant, bad_variant_access)
 {
@@ -41,3 +78,39 @@ TEST(variant, bad_variant_access)
     EXPECT_THROW(hj::get<std::string>(v), std::bad_variant_access);
 }
 #endif
+
+TEST(variant, reference_category)
+{
+    hj::variant<int, std::string> v = std::string("ref");
+
+    static_assert(
+        std::is_same_v<decltype(hj::get<std::string>(v)), std::string &>,
+        "get() must return lvalue reference for lvalue variant");
+
+    static_assert(std::is_same_v<decltype(hj::get<std::string>(std::move(v))),
+                                 std::string &&>,
+                  "get() must return rvalue reference for rvalue variant");
+
+    const auto &cv = v;
+    static_assert(
+        std::is_same_v<decltype(hj::get<std::string>(cv)), const std::string &>,
+        "get() must return const lvalue reference for const lvalue variant");
+}
+
+TEST(variant, traits_test)
+{
+    using MyVar = hj::variant<int, std::string, double>;
+
+    static_assert(hj::variant_size_v<MyVar> == 3, "Size mismatch");
+
+    static_assert(std::is_same_v<hj::variant_alternative_t<0, MyVar>, int>,
+                  "Alternative 0 should be int");
+
+    static_assert(
+        std::is_same_v<hj::variant_alternative_t<1, MyVar>, std::string>,
+        "Alternative 1 should be std::string");
+
+    using FirstType = hj::variant_alternative_t<0, MyVar>;
+    FirstType val   = 10;
+    ASSERT_EQ(val, 10);
+}

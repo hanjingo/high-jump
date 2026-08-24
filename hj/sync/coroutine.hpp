ea07fdc9b1aff97ae83f9b61819832a209a9744a
hanjingo <hehehunanchina@live.com>
2026-08-24 11:23:02 +0800

    upgrade coroutine v2

--- hj/sync/coroutine.hpp
+++ hj/sync/coroutine.hpp
@@ -8,7 +8,7 @@
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  withOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
@@ -19,36 +19,281 @@
 #define COROUTINE_HPP
 
 #include <boost/coroutine2/all.hpp>
+#include <boost/context/detail/config.hpp>
+#include <boost/context/stack_traits.hpp>
 #include <exception>
-#include <iostream>
+#include <functional>
+#include <utility>
+#include <stdexcept>
+#include <memory>
+#include <cstddef>
+#include <type_traits>
+#include <string>
 
 namespace hj
 {
 
-// See Also: https://www.boost.org/doc/libs/1_75_0/libs/coroutine2/doc/html/index.html
+struct coroutine_options
+{
+    static std::size_t default_size() noexcept
+    {
+        return boost::context::stack_traits::default_size();
+    }
 
-template <typename T>
-using coroutine = boost::coroutines2::coroutine<T>;
+    static std::size_t minimum_size() noexcept
+    {
+        return boost::context::stack_traits::minimum_size();
+    }
 
-using stack_alloc = boost::context::fixedsize_stack;
+    static std::size_t maximum_size() noexcept
+    {
+        return boost::context::stack_traits::maximum_size();
+    }
 
-}
+    std::size_t stack_size = default_size();
 
-#define __coroutine_cat(a, b) a##b
-#define _coroutine_cat(a, b) __coroutine_cat(a, b)
+    void validate() const
+    {
+        if(stack_size < minimum_size())
+        {
+            throw std::invalid_argument(
+                "coroutine_options: stack_size (" + std::to_string(stack_size)
+                + ") is smaller than minimum allowed size ("
+                + std::to_string(minimum_size()) + ")");
+        }
 
-#define COROUTINE(cmd)                                                         \
-    hj::coroutine<void>::pull_type __coroutine_cat(__coroutine__,              \
-                                                   __COUNTER__)(               \
-        [&](hj::coroutine<void>::push_type &) {                                \
-            try                                                                \
-            {                                                                  \
-                cmd;                                                           \
-            }                                                                  \
-            catch(const std::exception &e)                                     \
-            {                                                                  \
-                std::cerr << "Coroutine exception: " << e.what() << std::endl; \
-            }                                                                  \
-        });
+        if(boost::context::stack_traits::is_unbounded())
+        {
+            return;
+        }
 
-#endif
+        if(stack_size > maximum_size())
+        {
+            throw std::invalid_argument("coroutine_options: stack_size ("
+                                        + std::to_string(stack_size)
+                                        + ") exceeds maximum allowed size ("
+                                        + std::to_string(maximum_size()) + ")");
+        }
+    }
+};
+
+template <typename T = void>
+class coroutine
+{
+  public:
+    using boost_pull_type =
+        typename boost::coroutines2::coroutine<T>::pull_type;
+    using boost_push_type =
+        typename boost::coroutines2::coroutine<T>::push_type;
+
+    class yield_context
+    {
+      public:
+        explicit yield_context(boost_push_type &sink)
+            : _sink(sink)
+        {
+        }
+
+        yield_context(const yield_context &)            = delete;
+        yield_context &operator=(const yield_context &) = delete;
+        yield_context(yield_context &&)                 = delete;
+        yield_context &operator=(yield_context &&)      = delete;
+
+        template <typename U,
+                  typename = std::enable_if_t<!std::is_void_v<T>
+                                              && std::is_convertible_v<U, T>>>
+        void operator()(U &&value)
+        {
+            _sink(std::forward<U>(value));
+        }
+
+        template <typename Dummy = T,
+                  typename       = std::enable_if_t<std::is_void_v<Dummy>>>
+        void operator()()
+        {
+            _sink();
+        }
+
+      private:
+        boost_push_type &_sink;
+    };
+
+    using coroutine_fn      = std::function<void(yield_context &)>;
+    using exception_handler = std::function<void(const std::exception_ptr &)>;
+
+  private:
+    struct SharedState
+    {
+        std::exception_ptr exception_ptr{nullptr};
+    };
+
+  public:
+    coroutine() noexcept = default;
+
+    explicit coroutine(
+        coroutine_fn      fn,
+        exception_handler handler = [](const std::exception_ptr &) {})
+        : coroutine(std::move(fn), coroutine_options{}, std::move(handler))
+    {
+    }
+
+    explicit coroutine(
+        coroutine_fn      fn,
+        coroutine_options options,
+        exception_handler handler = [](const std::exception_ptr &) {})
+        : _state(std::make_shared<SharedState>())
+    {
+        options.validate();
+
+        _pull = std::make_unique<boost_pull_type>(
+            boost::context::fixedsize_stack(options.stack_size),
+            [state = _state, fn = std::move(fn), handler = std::move(handler)](
+                boost_push_type &sink) {
+                try
+                {
+                    yield_context ctx(sink);
+                    fn(ctx);
+                }
+                catch(const boost::context::detail::forced_unwind &)
+                {
+                    throw;
+                }
+                catch(...)
+                {
+                    state->exception_ptr = std::current_exception();
+                    if(handler)
+                    {
+                        try
+                        {
+                            handler(state->exception_ptr);
+                        }
+                        catch(...)
+                        {
+                        }
+                    }
+                }
+            });
+    }
+
+    coroutine(const coroutine &)            = delete;
+    coroutine &operator=(const coroutine &) = delete;
+
+    coroutine(coroutine &&other) noexcept
+        : _state(std::move(other._state))
+        , _pull(std::move(other._pull))
+    {
+    }
+
+    coroutine &operator=(coroutine &&other) noexcept
+    {
+        if(this != &other)
+        {
+            _pull.reset();
+
+            _state = std::move(other._state);
+            _pull  = std::move(other._pull);
+        }
+        return *this;
+    }
+
+    ~coroutine() noexcept = default;
+
+    void operator()()
+    {
+        if(_pull && *_pull)
+        {
+            (*_pull)();
+        }
+    }
+
+    void drain() noexcept
+    {
+        if(_pull)
+        {
+            try
+            {
+                while(*_pull)
+                {
+                    (*_pull)();
+                }
+            }
+            catch(...)
+            {
+            }
+        }
+    }
+
+    template <typename U = T, typename = std::enable_if_t<!std::is_void_v<U>>>
+    T get() const
+    {
+        if(has_exception())
+        {
+            throw std::logic_error(
+                "coroutine: cannot call get() after an unhandled exception was "
+                "thrown inside coroutine.");
+        }
+
+        if(_pull && *_pull)
+        {
+            return (*_pull).get();
+        }
+
+        throw std::logic_error(
+            "coroutine: cannot call get() on a completed coroutine without "
+            "value.");
+    }
+
+    bool     operator!() const { return !is_resumable(); }
+    explicit operator bool() const { return is_resumable(); }
+
+    bool has_value() const noexcept
+    {
+        if constexpr(std::is_void_v<T>)
+        {
+            return false;
+        } else
+        {
+            return (_pull && static_cast<bool>(*_pull));
+        }
+    }
+
+    bool is_resumable() const noexcept
+    {
+        return _pull && static_cast<bool>(*_pull);
+    }
+
+    bool done() const noexcept { return !is_resumable(); }
+
+    bool has_exception() const noexcept
+    {
+        return _state && _state->exception_ptr != nullptr;
+    }
+
+    std::exception_ptr exception() const noexcept
+    {
+        return _state ? _state->exception_ptr : nullptr;
+    }
+
+    void rethrow_exception() const
+    {
+        if(_state && _state->exception_ptr)
+        {
+            std::rethrow_exception(_state->exception_ptr);
+        }
+    }
+
+  private:
+    std::shared_ptr<SharedState>     _state;
+    std::unique_ptr<boost_pull_type> _pull;
+};
+
+} // namespace hj
+
+#define hj_coroutine_cat__(a, b) a##b
+#define hj_coroutine_cat_(a, b) hj_coroutine_cat__(a, b)
+
+#define HJ_COROUTINE(cmd)                                                      \
+    ::hj::coroutine<void> hj_coroutine_cat_(__coroutine__, __COUNTER__)(       \
+        [&](::hj::coroutine<void>::yield_context &) { cmd })
+
+#endif // COROUTINE_HPP
--- tests/coroutine_test.cpp
+++ tests/coroutine_test.cpp
@@ -1,102 +1,220 @@
 #include <gtest/gtest.h>
 #include <hj/sync/coroutine.hpp>
+#include <stdexcept>
+#include <type_traits>
 
-#ifndef __SANITIZE_ADDRESS__
+static_assert(!std::is_copy_constructible_v<hj::coroutine<void>::yield_context>,
+              "yield_context must not be copy constructible");
+static_assert(!std::is_copy_assignable_v<hj::coroutine<void>::yield_context>,
+              "yield_context must not be copy assignable");
+static_assert(!std::is_move_constructible_v<hj::coroutine<void>::yield_context>,
+              "yield_context must not be move constructible");
+static_assert(!std::is_move_assignable_v<hj::coroutine<void>::yield_context>,
+              "yield_context must not be move assignable");
 
-// void(*)(void)
-void f1(hj::coroutine<void>::push_type &out)
+void f1(hj::coroutine<void>::yield_context &ctx)
 {
     ASSERT_EQ(true, true);
+    ctx();
 }
 
-// void(*)(std::string)
-void f2(hj::coroutine<std::string>::pull_type &in)
+void f3(hj::coroutine<std::string>::yield_context &ctx)
 {
-    ASSERT_STREQ(in.get().c_str(), "hj");
-    in();
-
-    ASSERT_STREQ(in.get().c_str(), "c++");
-    in();
-
-    ASSERT_STREQ(in.get().c_str(), "");
+    ctx("hello");
+    ctx("world");
 }
 
-// std::string(*)(void)
-void f3(hj::coroutine<std::string>::push_type &out)
+TEST(coroutine, AbstractionAndLifecycle)
 {
-    out("hello");
-    out("world");
-}
+    hj::coroutine<void> co1(f1);
+    EXPECT_TRUE(co1.is_resumable());
+    co1();
+    EXPECT_TRUE(co1.done());
 
-// NOTE: NOT SUPPORT std::string(*)(std::string)
+    HJ_COROUTINE(ASSERT_EQ(true, true););
 
-TEST(coroutine, coroutine)
-{
-    // co1
-    hj::coroutine<void>::pull_type co1(f1);
-
-    hj::coroutine<void>::pull_type co1_lambda(
-        [](hj::coroutine<void>::push_type &out) {
-            ASSERT_EQ(true, true);
-            // std::cout << "lambda" << std::endl;
-        });
-
-    COROUTINE(ASSERT_EQ(true, true);
-              // std::cout << "COROUTINE" << std::endl;
-    );
-
-    // co2
-    hj::coroutine<std::string>::push_type co2(f2);
-    co2("hj");
-    co2("c++");
-
-    hj::coroutine<std::string>::push_type co2_lambda(
-        hj::stack_alloc(1 * 1024 * 1024),
-        [](hj::coroutine<std::string>::pull_type &in) {
-            ASSERT_STREQ(in.get().c_str(), "hj");
-            in();
-
-            ASSERT_STREQ(in.get().c_str(), "c++");
-            in();
-
-            ASSERT_STREQ(in.get().c_str(), ""); // get() -> std::string()
-        });
-    co2_lambda("hj");
-    co2_lambda("c++");
-
-
-    hj::coroutine<std::string>::pull_type co3(f3);
+    hj::coroutine<std::string> co3(f3);
     ASSERT_STREQ(co3.get().c_str(), "hello");
-    co3(); // ["hello", "world"] -> ["world"]
+    EXPECT_TRUE(co3);
+
+    co3();
     ASSERT_STREQ(co3.get().c_str(), "world");
-    co3();                               // ["world"] -> []
-    ASSERT_STREQ(co3.get().c_str(), ""); // get() -> std::string()
+    EXPECT_TRUE(co3);
 
-    hj::coroutine<std::string>::pull_type co3_lambda(
-        [](hj::coroutine<std::string>::push_type &out) {
-            out("hello");
-            out("world");
-        });
-    ASSERT_STREQ(co3_lambda.get().c_str(), "hello");
-    co3_lambda(); // ["hello", "world"] -> ["world"]
-    ASSERT_STREQ(co3_lambda.get().c_str(), "world");
-    co3_lambda();                               // ["world"] -> []
-    ASSERT_STREQ(co3_lambda.get().c_str(), ""); // get() -> std::string()
+    co3();
+    EXPECT_FALSE(co3);
 
-    hj::coroutine<int>::pull_type co4_lambda(
-        [](hj::coroutine<int>::push_type &out) {
-            out(1);
-            out(2);
-            out(3);
-        });
+    hj::coroutine<int> co4_lambda([](hj::coroutine<int>::yield_context &ctx) {
+        ctx(1);
+        ctx(2);
+    });
+
     ASSERT_EQ(co4_lambda.get(), 1);
-    co4_lambda(); // [1, 2, 3] -> [2, 3]
+    co4_lambda();
     ASSERT_EQ(co4_lambda.get(), 2);
-    co4_lambda(); // [2, 3] -> [3]
-    ASSERT_EQ(co4_lambda.get(), 3);
-    co4_lambda(); // [3] -> [3]
-    ASSERT_EQ(co4_lambda.get(), 3);
-    ASSERT_EQ(co4_lambda.get(), 3);
+    co4_lambda();
+    EXPECT_FALSE(co4_lambda);
+    EXPECT_THROW(co4_lambda.get(), std::logic_error);
 }
 
-#endif
+TEST(coroutine, EmptyAndStateSemantic)
+{
+    hj::coroutine<void> co(
+        [](hj::coroutine<void>::yield_context &ctx) { ctx(); });
+
+    EXPECT_TRUE(co.is_resumable());
+    EXPECT_FALSE(co.done());
+
+    co();
+
+    EXPECT_FALSE(co.is_resumable());
+    EXPECT_TRUE(co.done());
+    EXPECT_FALSE(co.has_exception());
+}
+
+TEST(coroutine, DestroySuspendedCoroutineDoesNotResume)
+{
+    bool stack_frame_dtor_called = false;
+    bool resumed_after_yield     = false;
+
+    {
+        hj::coroutine<void> co([&](hj::coroutine<void>::yield_context &ctx) {
+            struct ScopedGuard
+            {
+                bool *flag;
+                ~ScopedGuard() { *flag = true; }
+            } guard{&stack_frame_dtor_called};
+
+            ctx();
+
+            resumed_after_yield = true;
+        });
+
+        EXPECT_TRUE(co.is_resumable());
+    }
+
+    EXPECT_FALSE(resumed_after_yield);
+    EXPECT_TRUE(stack_frame_dtor_called);
+}
+
+TEST(coroutine, MoveAssignmentDestroysTargetWithoutResume)
+{
+    bool target_dtor_called = false;
+    bool target_resumed     = false;
+
+    hj::coroutine<void> target([&](hj::coroutine<void>::yield_context &ctx) {
+        struct ScopedGuard
+        {
+            bool *flag;
+            ~ScopedGuard() { *flag = true; }
+        } guard{&target_dtor_called};
+
+        ctx();
+        target_resumed = true;
+    });
+
+    hj::coroutine<void> source(
+        [](hj::coroutine<void>::yield_context &ctx) { ctx(); });
+
+    EXPECT_TRUE(target.is_resumable());
+    EXPECT_TRUE(source.is_resumable());
+
+    target = std::move(source);
+
+    EXPECT_TRUE(target_dtor_called);
+    EXPECT_FALSE(target_resumed);
+    EXPECT_TRUE(target.is_resumable());
+    EXPECT_FALSE(source);
+}
+
+TEST(coroutine, DestroyCoroutineWithExceptionDoesNotThrowOrTerminate)
+{
+    EXPECT_NO_THROW({
+        hj::coroutine<void> co([](hj::coroutine<void>::yield_context &) {
+            throw std::runtime_error("unhandled exception inside coroutine");
+        });
+
+        co();
+        EXPECT_TRUE(co.has_exception());
+    });
+
+    EXPECT_NO_THROW({
+        hj::coroutine<void> co([](hj::coroutine<void>::yield_context &ctx) {
+            ctx();
+            throw std::logic_error("error after resume");
+        });
+    });
+}
+
+TEST(coroutine, ExceptionPropagation)
+{
+    hj::coroutine<void> co([](hj::coroutine<void>::yield_context &) {
+        throw std::runtime_error("coroutine test failure");
+    });
+
+    co();
+
+    EXPECT_TRUE(co.has_exception());
+    EXPECT_NE(co.exception(), nullptr);
+    EXPECT_THROW({ co.rethrow_exception(); }, std::runtime_error);
+}
+
+TEST(coroutine, HandlerExceptionIsolation)
+{
+    bool handler_called = false;
+
+    hj::coroutine<void> co(
+        [](hj::coroutine<void>::yield_context &) {
+            throw std::runtime_error("original error");
+        },
+        [&handler_called](const std::exception_ptr &) {
+            handler_called = true;
+            throw std::runtime_error("handler error");
+        });
+
+    co();
+
+    EXPECT_TRUE(handler_called);
+    EXPECT_TRUE(co.has_exception());
+    EXPECT_THROW({ co.rethrow_exception(); }, std::runtime_error);
+}
+
+TEST(coroutine, MoveSemantics)
+{
+    hj::coroutine<void> co_a(
+        [](hj::coroutine<void>::yield_context &ctx) { ctx(); });
+
+    EXPECT_TRUE(co_a);
+
+    hj::coroutine<void> co_b(std::move(co_a));
+    EXPECT_FALSE(co_a);
+    EXPECT_TRUE(co_b);
+
+    hj::coroutine<void> co_c;
+    co_c = std::move(co_b);
+    EXPECT_FALSE(co_b);
+    EXPECT_TRUE(co_c);
+}
+
+TEST(coroutine, MoveThenException)
+{
+    hj::coroutine<void> a(
+        [](auto &) { throw std::runtime_error("failure after move"); });
+
+    hj::coroutine<void> b(std::move(a));
+    b();
+
+    EXPECT_TRUE(b.has_exception());
+    EXPECT_THROW(b.rethrow_exception(), std::runtime_error);
+}
+
+TEST(coroutine, GetAfterExceptionSemantic)
+{
+    hj::coroutine<int> co([](auto &) { throw std::runtime_error("fail"); });
+
+    co();
+    EXPECT_TRUE(co.has_exception());
+
+    EXPECT_THROW(co.get(), std::logic_error);
+    EXPECT_THROW(co.rethrow_exception(), std::runtime_error);
+}

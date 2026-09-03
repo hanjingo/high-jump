bc7b04d1492e35938e14635835a6e628cf24d86d
hanjingo <hehehunanchina@live.com>
2026-09-03 18:04:10 +0800

    upgrade asm v1

--- hj/os/asm.h
+++ hj/os/asm.h
@@ -16,58 +16,182 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef ASM_H
-#define ASM_H
+#ifndef HJ_OS_ASM_H
+#define HJ_OS_ASM_H
 
+/* Compiler Level */
+#define HJ_COMPILER_GCC 0
+#define HJ_COMPILER_CLANG 0
+#define HJ_COMPILER_MSVC 0
 
-#ifdef __cplusplus
-extern "C" {
+/* Architecture Level */
+#define HJ_ARCH_X86 0
+#define HJ_ARCH_X86_32 0
+#define HJ_ARCH_X86_64 0
+
+#define HJ_ARCH_ARM 0
+#define HJ_ARCH_ARM32 0
+#define HJ_ARCH_ARM64 0
+
+#define HJ_ARCH_RISCV 0
+#define HJ_ARCH_RISCV32 0
+#define HJ_ARCH_RISCV64 0
+
+/* Feature Level */
+#define HJ_HAS_EXTENDED_INLINE_ASM 0
+#define HJ_HAS_MSVC_INLINE_ASM 0 // Legacy MSVC x86-32 bit only
+#define HJ_HAS_INLINE_ASM 0
+#define HJ_HAS_INTRINSICS 1
+
+/* -------------------------------------------------------------------------- */
+/* Compiler Detection                                                         */
+/* -------------------------------------------------------------------------- */
+
+#if defined(__clang__)
+#undef HJ_COMPILER_CLANG
+#define HJ_COMPILER_CLANG 1
+#elif defined(__GNUC__)
+#undef HJ_COMPILER_GCC
+#define HJ_COMPILER_GCC 1
 #endif
 
 #if defined(_MSC_VER)
-#if defined(_M_IX86)
-#define ASM_BEGIN __asm {
-#define ASM_END }
-#define ASM_INLINE __asm
-#define ASM_VOLATILE
-#define ASM(code) __asm { code }
-#define ASM_V(code) __asm { code }
-#else
-#pragma message("Warning: MSVC x64/ARM does not support inline assembly.")
-#define ASM_BEGIN
-#define ASM_END
-#define ASM_INLINE
-#define ASM_VOLATILE
-#define ASM(code)
-#define ASM_V(code)
-#endif
-#elif defined(__GNUC__) || defined(__clang__)
-#if defined(__ASSEMBLER__)
-#define ASM_BEGIN
-#define ASM_END
-#define ASM_INLINE
-#define ASM_VOLATILE
-#define ASM(code)
-#define ASM_V(code)
-#else
-#define ASM_BEGIN   asm (
-#define ASM_END     )
-#define ASM_INLINE asm
-#define ASM_VOLATILE asm volatile
-#define ASM(code) asm(code)
-#define ASM_V(code) asm volatile(code)
-#endif
-#else
-#define ASM_BEGIN
-#define ASM_END
-#define ASM_INLINE
-#define ASM_VOLATILE
-#define ASM(code)
-#define ASM_V(code)
+#undef HJ_COMPILER_MSVC
+#define HJ_COMPILER_MSVC 1
 #endif
 
-#ifdef __cplusplus
-}
+/* -------------------------------------------------------------------------- */
+/* Architecture Detection                                                     */
+/* -------------------------------------------------------------------------- */
+
+#if defined(__i386__) || defined(_M_IX86)
+#undef HJ_ARCH_X86_32
+#define HJ_ARCH_X86_32 1
+#undef HJ_ARCH_X86
+#define HJ_ARCH_X86 1
+#elif defined(__x86_64__) || defined(_M_X64) || defined(_M_AMD64)
+#undef HJ_ARCH_X86_64
+#define HJ_ARCH_X86_64 1
+#undef HJ_ARCH_X86
+#define HJ_ARCH_X86 1
 #endif
 
-#endif // ASM_H
+#if defined(__aarch64__) || defined(_M_ARM64)
+#undef HJ_ARCH_ARM64
+#define HJ_ARCH_ARM64 1
+#undef HJ_ARCH_ARM
+#define HJ_ARCH_ARM 1
+#elif defined(__arm__) || defined(_M_ARM)
+#undef HJ_ARCH_ARM32
+#define HJ_ARCH_ARM32 1
+#undef HJ_ARCH_ARM
+#define HJ_ARCH_ARM 1
+#endif
+
+#if defined(__riscv)
+#undef HJ_ARCH_RISCV
+#define HJ_ARCH_RISCV 1
+#if __riscv_xlen == 64
+#undef HJ_ARCH_RISCV64
+#define HJ_ARCH_RISCV64 1
+#elif __riscv_xlen == 32
+#undef HJ_ARCH_RISCV32
+#define HJ_ARCH_RISCV32 1
+#endif
+#endif
+
+/* -------------------------------------------------------------------------- */
+/* Assembly Feature Detection                                                 */
+/* -------------------------------------------------------------------------- */
+
+/* GCC 风格 Extended Inline ASM */
+#if (HJ_COMPILER_GCC || HJ_COMPILER_CLANG) && !defined(__ASSEMBLER__)
+#undef HJ_HAS_EXTENDED_INLINE_ASM
+#define HJ_HAS_EXTENDED_INLINE_ASM 1
+#endif
+
+/* MSVC 传统 Inline ASM (仅 Legacy 32位 MSVC x86 支持) */
+#if HJ_COMPILER_MSVC && HJ_ARCH_X86_32
+#undef HJ_HAS_MSVC_INLINE_ASM
+#define HJ_HAS_MSVC_INLINE_ASM 1
+#endif
+
+#if HJ_HAS_EXTENDED_INLINE_ASM || HJ_HAS_MSVC_INLINE_ASM
+#undef HJ_HAS_INLINE_ASM
+#define HJ_HAS_INLINE_ASM 1
+#endif
+
+/* -------------------------------------------------------------------------- */
+/* Assembly Modifiers (Only defined if the current platform supports assembly)   */
+/* -------------------------------------------------------------------------- */
+
+#if HJ_HAS_EXTENDED_INLINE_ASM
+#define HJ_ASM __asm__
+#define HJ_ASM_VOLATILE __asm__ __volatile__
+#elif HJ_HAS_MSVC_INLINE_ASM
+#define HJ_ASM __asm
+#define HJ_ASM_VOLATILE __asm
+#endif
+/* 注意：不支持内联汇编的平台故意不定义 HJ_ASM / HJ_ASM_VOLATILE，
+   防止代码在未受保护的情况下使用裸汇编宏产生错误的静默展开。 */
+
+/* -------------------------------------------------------------------------- */
+/* Intrinsics Headers                                                          */
+/* -------------------------------------------------------------------------- */
+
+#if HJ_COMPILER_MSVC || defined(__clang_cli__)
+#include <intrin.h>
+#elif HJ_COMPILER_GCC || HJ_COMPILER_CLANG
+#if HJ_ARCH_X86
+#include <x86intrin.h>
+#elif HJ_ARCH_ARM
+#include <arm_acle.h>
+#endif
+#endif
+
+/* -------------------------------------------------------------------------- */
+/* Cross-Platform CPU Primitives & Barriers                                     */
+/* -------------------------------------------------------------------------- */
+#if HJ_COMPILER_MSVC
+#define HJ_COMPILER_BARRIER() _ReadWriteBarrier()
+#elif HJ_HAS_EXTENDED_INLINE_ASM
+#define HJ_COMPILER_BARRIER() __asm__ __volatile__("" ::: "memory")
+#else
+#define HJ_COMPILER_BARRIER() ((void) 0)
+#endif
+
+#if HJ_ARCH_X86
+#if HJ_COMPILER_MSVC
+#define HJ_ASM_PAUSE() _mm_pause()
+#elif HJ_HAS_EXTENDED_INLINE_ASM
+#define HJ_ASM_PAUSE() __asm__ __volatile__("pause")
+#else
+#define HJ_ASM_PAUSE() ((void) 0)
+#endif
+#elif HJ_ARCH_ARM
+#if HJ_COMPILER_MSVC
+#define HJ_ASM_PAUSE() __yield()
+#elif HJ_HAS_EXTENDED_INLINE_ASM
+#define HJ_ASM_PAUSE() __asm__ __volatile__("yield")
+#else
+#define HJ_ASM_PAUSE() ((void) 0)
+#endif
+#elif HJ_ARCH_RISCV
+#if HJ_HAS_EXTENDED_INLINE_ASM && defined(__riscv_zihintpause)
+#define HJ_ASM_PAUSE() __asm__ __volatile__("pause")
+#else
+#define HJ_ASM_PAUSE() ((void) 0)
+#endif
+#else
+#define HJ_ASM_PAUSE() ((void) 0)
+#endif
+
+#if HJ_COMPILER_MSVC
+#define HJ_ASM_NOP() __nop()
+#elif HJ_HAS_EXTENDED_INLINE_ASM
+#define HJ_ASM_NOP() __asm__ __volatile__("nop")
+#else
+#define HJ_ASM_NOP() ((void) 0)
+#endif
+
+#endif // HJ_OS_ASM_H
--- tests/asm_test.cpp
+++ tests/asm_test.cpp
@@ -1,71 +1,102 @@
 #include <gtest/gtest.h>
 #include <hj/os/asm.h>
 
-TEST(asm, asm_macro_basic)
+TEST(asm, feature_detection)
 {
-    int a = 1, b = 2, c = 0;
 #if defined(_MSC_VER) && defined(_M_IX86)
-    ASM_BEGIN
-    mov eax, a add eax, b mov c, eax ASM_END ASSERT_EQ(c, 3);
-// Only use the x86 inline-asm variant when compiling for x86 (32/64-bit)
-// Avoid trying to compile x86 registers (like eax) on non-x86 platforms
-#elif (defined(__GNUC__) || defined(__clang__)) && (defined(__i386__) || defined(__x86_64__))
-    int result = 0;
-    ASM_VOLATILE(
-        "movl %[a], %%eax; addl %[b], %%eax; movl %%eax, %[c]" : [c] "=r"(
-            result) : [a] "r"(a),
-        [b] "r"(b) : "eax");
-    ASSERT_EQ(result, 3);
+    EXPECT_TRUE(HJ_HAS_MSVC_INLINE_ASM);
+    EXPECT_TRUE(HJ_HAS_INLINE_ASM);
+#elif defined(__GNUC__) || defined(__clang__)
+    EXPECT_TRUE(HJ_HAS_EXTENDED_INLINE_ASM);
+    EXPECT_TRUE(HJ_HAS_INLINE_ASM);
 #else
-    SUCCEED();
+    EXPECT_FALSE(HJ_HAS_INLINE_ASM);
+#endif
+
+#if defined(_M_X64) || defined(__x86_64__)
+    EXPECT_TRUE(HJ_ARCH_X86_64);
+    EXPECT_TRUE(HJ_ARCH_X86);
+#elif defined(_M_IX86) || defined(__i386__)
+    EXPECT_TRUE(HJ_ARCH_X86_32);
+    EXPECT_TRUE(HJ_ARCH_X86);
+#endif
+
+#if defined(__aarch64__) || defined(_M_ARM64)
+    EXPECT_TRUE(HJ_ARCH_ARM64);
+    EXPECT_TRUE(HJ_ARCH_ARM);
+#elif defined(__arm__) || defined(_M_ARM)
+    EXPECT_TRUE(HJ_ARCH_ARM32);
+    EXPECT_TRUE(HJ_ARCH_ARM);
+#endif
+
+#if defined(__riscv)
+    EXPECT_TRUE(HJ_ARCH_RISCV);
+#if __riscv_xlen == 64
+    EXPECT_TRUE(HJ_ARCH_RISCV64);
+#elif __riscv_xlen == 32
+    EXPECT_TRUE(HJ_ARCH_RISCV32);
+#endif
 #endif
 }
 
-TEST(asm, asm_macro_empty)
+TEST(asm, compiler_barrier_memory_flush)
 {
-    ASM("");
-    ASM_V("");
+    int           memory_var = 100;
+    volatile int *p          = &memory_var;
+
+    *p = 200;
+
+    HJ_COMPILER_BARRIER();
+
+    EXPECT_EQ(*p, 200);
+
+    *p = 300;
+    HJ_COMPILER_BARRIER();
+
+    EXPECT_EQ(memory_var, 300);
+}
+
+TEST(asm, cpu_pause)
+{
+    HJ_ASM_PAUSE();
     SUCCEED();
 }
 
-TEST(asm, platform_basic)
+TEST(asm, platform_inline_asm_execution)
 {
-#if defined(_MSC_VER) && defined(_M_IX86)
-    // MSVC x86
     int a = 1, b = 2, c = 0;
-    ASM_BEGIN
-    mov eax, a add eax, b mov c, eax ASM_END ASSERT_EQ(c, 3);
-#elif defined(__GNUC__) && defined(__x86_64__)
-    // GCC x86_64
-    int a = 1, b = 2, c = 0;
-    ASM_VOLATILE(
-        "movl %[a], %%eax; addl %[b], %%eax; movl %%eax, %[c]" : [c] "=r"(
-            c) : [a] "r"(a),
-        [b] "r"(b) : "eax");
-    ASSERT_EQ(c, 3);
-#elif defined(__aarch64__)
-    // ARM64
-    int a = 1, b = 2, c = 0;
-    ASM_VOLATILE("add %[c], %[a], %[b]" : [c] "=r"(c) : [a] "r"(a), [b] "r"(b));
-    ASSERT_EQ(c, 3);
+
+#if HJ_HAS_EXTENDED_INLINE_ASM
+
+#if HJ_ARCH_X86
+    HJ_ASM_VOLATILE("addl %[b], %[a]" : [a] "+r"(a) : [b] "r"(b));
+    c = a;
+    EXPECT_EQ(c, 3);
+
+#elif HJ_ARCH_ARM64
+    HJ_ASM_VOLATILE("add %[c], %[a], %[b]" : [c] "=r"(c) : [a] "r"(a),
+                    [b] "r"(b));
+    EXPECT_EQ(c, 3);
+
+#elif HJ_ARCH_ARM32
+    HJ_ASM_VOLATILE("add %[c], %[a], %[b]" : [c] "=r"(c) : [a] "r"(a),
+                    [b] "r"(b));
+    EXPECT_EQ(c, 3);
+
+#elif HJ_ARCH_RISCV
+    HJ_ASM_VOLATILE("add %[c], %[a], %[b]" : [c] "=r"(c) : [a] "r"(a),
+                    [b] "r"(b));
+    EXPECT_EQ(c, 3);
+
 #else
-    SUCCEED();
+    c = a + b;
+    EXPECT_EQ(c, 3);
 #endif
-}
 
-TEST(asm, asm_empty)
-{
-    ASM("");
-    ASM_V("");
-    SUCCEED();
-}
-
-TEST(asm, asm_invalid)
-{
-#if defined(_MSC_VER) && defined(_M_IX86)
-    // ASM_BEGIN invalid_instruction ASM_END;
-    SUCCEED();
+#elif HJ_HAS_MSVC_INLINE_ASM
+    HJ_ASM{mov eax, a add eax, b mov c, eax} EXPECT_EQ(c, 3);
 #else
-    SUCCEED();
+    c = a + b;
+    EXPECT_EQ(c, 3);
 #endif
 }
